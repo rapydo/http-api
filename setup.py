53de7c11a490d14719946dde8ff7b73be6113dce
pdonorio <tempo.pause@gmail.com>
2017-06-15 16:04:47 +0200

    releasing rapydo_http-0.2.3.tar.gz

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "bravado-core",
         "swagger-spec-validator",
         # Rapydo framework
-        "rapydo-utils==0.4.2",
+        "rapydo-utils==0.4.3",
         # TODO: complete this list
         # from requirements in backend/requirements.txt
     ]
