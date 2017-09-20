3f4d228517b761edbe50704fe0004fb08b14d0bc
Mattia D'Antonio <m.dantonio@cineca.it>
2017-09-20 08:33:28 +0200

    Trying to fix the missing coveralls by starting the stack

--- scripts/test_and_coverage.sh
+++ scripts/test_and_coverage.sh
@@ -23,10 +23,11 @@ echo "Current version: $CURRENT_VERSION"
 if [ ! -d "core" ]; then
     git clone https://github.com/rapydo/core.git
 fi
-
 cd core
+
+COV_DIR="coverage_files"
 mkdir -p data
-mkdir -p coverage_files
+mkdir -p $COV_DIR
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "checkout $TRAVIS_BRANCH"
@@ -49,10 +50,10 @@ if [ "$PROJECT" != "COVERAGE" ]; then
 	rapydo --project ${PROJECT} shell backend --command 'restapi tests --wait --core'
 
 	# Sync the coverage file to S3, to be available for the next stage
-	docker cp ${PROJECT}_backend_1:/code/.coverage coverage_files/.coverage.${PROJECT}
+	docker cp ${PROJECT}_backend_1:/code/.coverage $COV_DIR/.coverage.${PROJECT}
 
 	aws --endpoint-url $S3_HOST s3api create-bucket --bucket http-api-${TRAVIS_BUILD_ID}
-	aws --endpoint-url $S3_HOST s3 sync coverage_files s3://http-api-${TRAVIS_BUILD_ID}
+	aws --endpoint-url $S3_HOST s3 sync $COV_DIR s3://http-api-${TRAVIS_BUILD_ID}
 
 else
 
@@ -60,23 +61,21 @@ else
 
 	# Download sub-repos (build templates are required)
 	rapydo --project ${PROJECT} init --skip-bower 
+	rapydo --project ${PROJECT} --services backend start
+	docker ps -a
+	# Build the backend image and execute coveralls
+	# rapydo --services backend --project ${PROJECT} build
 
 	# Sync coverage files from previous stages
-	aws --endpoint-url $S3_HOST s3 sync s3://http-api-${TRAVIS_BUILD_ID} coverage_files
+	aws --endpoint-url $S3_HOST s3 sync s3://http-api-${TRAVIS_BUILD_ID} $COV_DIR
 
     # Combine all coverage files to compute thefinal coverage
-	cd coverage_files
+	cd $COV_DIR
 	ls .coverage*
 	coverage combine
-	ls .coverage*
 	cd -
 
-	# Build the backend image and execute coveralls
-	rapydo --services backend --project ${PROJECT} build
-
-	pwd
-	ls
-	cat .coveralls.yml
+	cp $COV_DIR/.coverage .
 	docker run -it -v $(pwd):/repo -w /repo template/backend:template coveralls
 
 
