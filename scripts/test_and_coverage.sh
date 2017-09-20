076f0ec558c5f3533c6e3d08960cb048706c3657
Mattia D'Antonio <m.dantonio@cineca.it>
2017-09-20 07:50:11 +0200

    BUG fix coverage script

--- scripts/test_and_coverage.sh
+++ scripts/test_and_coverage.sh
@@ -27,25 +27,28 @@ fi
 cd core
 mkdir -p data
 mkdir -p coverage_files
-# chmod -R o+Xw projects
-# echo "checking permissions:"
-# ls -ld projects/$CORE_PROJECT/
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "checkout $TRAVIS_BRANCH"
     git checkout $TRAVIS_BRANCH
 fi
 
+# Save credentials for S3 storage
 aws configure set aws_access_key_id $S3_USER 
 aws configure set aws_secret_access_key $S3_PWD
 
 if [ "$PROJECT" != "COVERAGE" ]; then
+
+	# Let's init and start the stack for the configured PROJECT
 	rapydo --project ${PROJECT} init --skip-bower 
 
 	rapydo --project ${PROJECT} start
 	docker ps -a
 
+	# Test API and calculate coverage
 	rapydo --project ${PROJECT} shell backend --command 'restapi tests --wait --core'
+
+	# Sync the coverage file to S3, to be available for the next stage
 	docker cp ${PROJECT}_backend_1:/code/.coverage coverage_files/.coverage.${PROJECT}
 
 	aws --endpoint-url $S3_HOST s3api create-bucket --bucket http-api-${TRAVIS_BUILD_ID}
@@ -55,15 +58,20 @@ else
 
 	PROJECT="template"
 
+	# Download sub-repos (build templates are required)
 	rapydo --project ${PROJECT} init --skip-bower 
 
+	# Sync coverage files from previous stages
 	aws --endpoint-url $S3_HOST s3 sync s3://http-api-${TRAVIS_BUILD_ID} coverage_files
 
+    # Combine all coverage files to compute thefinal coverage
 	cd coverage_files
 	ls .coverage*
-
 	coverage combine
+	ls .coverage*
+	cd -
 
+	# Build the backend image and execute coveralls
 	rapydo --services backend --project ${PROJECT} build
 	docker run -it -v $(pwd):/repo -w /repo template/backend:template coveralls
 
