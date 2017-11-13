a2a79ab1d8f81061281dcf14b2bc51b22f65b5d4
Mattia D'Antonio <m.dantonio@cineca.it>
2017-11-13 08:05:17 +0100

    Moving test utilities from nose2 to pytest

--- restapi/tests/__init__.py
+++ restapi/tests/__init__.py
@@ -30,6 +30,91 @@ class ParsedResponse(object):
 
 class BaseTests():
 
+    def save(self, variable, value, read_only=False):
+        """
+            Save a variable in the class, to be re-used in further tests
+            In read_only mode the variable cannot be rewritten
+        """
+        if hasattr(self.__class__, variable):
+            data = getattr(self.__class__, variable)
+            if "read_only" in data and data["read_only"]:
+                pytest.fail(
+                    "Cannot overwrite a read_only variable [%s]" % variable
+                )
+
+        data = {'value': value, 'read_only': read_only}
+        setattr(self.__class__, variable, data)
+
+    def get(self, variable):
+        """
+            Retrieve a previously stored variable using the .save method
+        """
+        if hasattr(self.__class__, variable):
+            data = getattr(self.__class__, variable)
+            if "value" in data:
+                return data["value"]
+
+        raise AttributeError("Class variable %s not found" % variable)
+        return None
+
+    def get_specs(self):
+        """
+            Retrieve Swagger definition by calling API/specs endpoint
+        """
+        r = self.app.get(API_URI + '/specs')
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        content = json.loads(r.data.decode('utf-8'))
+        return content
+
+    def get_definition(self, specs, endpoint):
+        """
+            Given a swagger specs this method extracts a swagger definition
+            for a specific endpoint. The endpoint is expected to have variables
+            defined following swagger rules, e.g /path/{variable}
+        """
+        mapping = "%s/%s" % (API_URL, endpoint)
+
+        assert mapping in specs["paths"]
+        return specs["paths"][mapping]
+
+    def getInputSchema(self, endpoint, headers):
+        """
+            Retrieve a swagger-like data schema associated with a endpoint
+        """
+        r = self.app.get(API_URI + '/schemas/' + endpoint, headers=headers)
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        content = json.loads(r.data.decode('utf-8'))
+        return content['Response']['data']
+
+    def getDynamicInputSchema(self, endpoint, headers):
+        """
+            Retrieve a dynamic data schema associated with a endpoint
+        """
+
+        data = {"get_schema": 1}
+        r = self.app.post(
+            "%s/%s" % (API_URI, endpoint),
+            data=data,
+            headers=headers)
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        content = json.loads(r.data.decode('utf-8'))
+        return content['Response']['data']
+
+    def get_content(self, response):
+        content, err, meta, code = get_content_from_response(response)
+
+        # Since unittests use class object and not instances
+        # This is the only workaround to set a persistent variable:
+        # abuse of the __class__ property
+
+        self.__class__.latest_response = {
+            "metadata": meta,
+            "content": content,
+            "errors": err,
+            "status": code,
+        }
+        return content
+
     def do_login(self, client, USER, PWD,
                  status_code=hcodes.HTTP_OK_BASIC,
                  error=None, **kwargs):
@@ -78,47 +163,10 @@ class BaseTests():
                 token = data.get('token', '')
         return {'Authorization': 'Bearer ' + token}, token
 
-    def save(self, variable, value, read_only=False):
-        """
-            Save a variable in the class, to be re-used in further tests
-            In read_only mode the variable cannot be rewritten
-        """
-        if hasattr(self.__class__, variable):
-            data = getattr(self.__class__, variable)
-            if "read_only" in data and data["read_only"]:
-                pytest.fail(
-                    "Cannot overwrite a read_only variable [%s]" % variable
-                )
-
-        data = {'value': value, 'read_only': read_only}
-        setattr(self.__class__, variable, data)
-
-    def get(self, variable):
-        """
-            Retrieve a previously stored variable using the .save method
-        """
-        if hasattr(self.__class__, variable):
-            data = getattr(self.__class__, variable)
-            if "value" in data:
-                return data["value"]
-
-        raise AttributeError("Class variable %s not found" % variable)
-        return None
-
-    def get_content(self, response):
-        content, err, meta, code = get_content_from_response(response)
-
-        # Since unittests use class object and not instances
-        # This is the only workaround to set a persistent variable:
-        # abuse of the __class__ property
-
-        self.__class__.latest_response = {
-            "metadata": meta,
-            "content": content,
-            "errors": err,
-            "status": code,
-        }
-        return content
+    def get_profile(self, headers):
+        r = self.app.get(AUTH_URI + '/profile', headers=headers)
+        content = json.loads(r.data.decode('utf-8'))
+        return content['Response']['data']
 
     def get_celery(self, app):
 
@@ -211,3 +259,65 @@ class BaseTests():
         for r in relationships:
             if not hasattr(response[0], "_" + r):
                 pytest.fail("Missing relationship: %s" % r)
+
+    def buildData(self, schema):
+        """
+            Input: a Swagger-like schema
+            Output: a dictionary of random data
+        """
+        data = {}
+        for d in schema:
+
+            key = d["name"]
+            type = d["type"]
+            format = d.get("format", "")
+            default = d.get("default", None)
+            custom = d.get("custom", {})
+            autocomplete = custom.get("autocomplete", False)
+            test_with = custom.get("test_with", None)
+
+            if autocomplete and test_with is None:
+                continue
+
+            value = None
+            if test_with is not None:
+                value = test_with
+            elif 'enum' in d:
+                if default is not None:
+                    value = default
+                elif len(d["enum"]) > 0:
+                    # get first key
+                    for value in d["enum"][0]:
+                        break
+                else:
+                    value = "NOT_FOUND"
+            elif type == "int":
+                value = random.randrange(0, 1000, 1)
+            elif format == "date":
+                value = "1969-07-20"  # 20:17:40 UTC
+            elif type == "multi_section":
+                continue
+            else:
+                value = self.randomString()
+
+            data[key] = value
+
+        return data
+
+    def getPartialData(self, schema, data):
+        """
+            Following directives contained in the schema and
+            taking as input a pre-built data dictionary, this method
+            remove one of the required fields from data
+        """
+        partialData = data.copy()
+        for d in schema:
+            if not d['required']:
+                continue
+
+            # key = d["key"]
+            key = d["name"]
+
+            del partialData[key]
+            return partialData
+        return None
