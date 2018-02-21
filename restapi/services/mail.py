d7b4b4ce913e23f6a06ab87c0b2759de9d91bee9
Mattia <m.dantonio81@gmail.com>
2018-02-21 10:50:28 +0100

    [rapydo/issues#36] HTML emails

--- restapi/services/mail.py
+++ restapi/services/mail.py
@@ -7,14 +7,8 @@ from utilities.logs import get_logger
 log = get_logger(__name__)
 
 
-def send_mail_is_active():
-    host = os.environ.get("SMTP_HOST")
-    port = os.environ.get("SMTP_PORT")
-
-    return host is not None and port is not None
-
-
-def send_mail(body, subject, to_address=None, from_address=None):
+def send_mail(
+        body, subject, to_address=None, from_address=None, plain_body=None):
 
     try:
         host = os.environ.get("SMTP_HOST")
@@ -23,21 +17,29 @@ def send_mail(body, subject, to_address=None, from_address=None):
         password = os.environ.get("SMTP_PASSWORD")
 
         if from_address is None:
-            from_address = os.environ.get("SMTP_NOREPLY")
-
-        if from_address is None:
             from_address = os.environ.get("SMTP_ADMIN")
 
         if to_address is None:
             to_address = os.environ.get("SMTP_ADMIN")
 
-        return send(
-            body, subject, to_address, from_address,
-            smtp_host=host,
-            smtp_port=port,
-            username=username,
-            password=password
-        )
+        if plain_body is None:
+            return send(
+                body, subject, to_address, from_address,
+                smtp_host=host,
+                smtp_port=port,
+                username=username,
+                password=password
+            )
+        else:
+            return send(
+                body, subject, to_address, from_address,
+                smtp_host=host,
+                smtp_port=port,
+                username=username,
+                password=password,
+                html=True,
+                plain_body=plain_body
+            )
 
     except BaseException as e:
         log.error(str(e))
