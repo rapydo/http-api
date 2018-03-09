3865774236779296e9ba49656c924e818001bd65
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.it>
2018-03-09 10:20:39 +0100

    examples on iclient added for metadata irule

--- restapi/flask_ext/flask_irods/client.py
+++ restapi/flask_ext/flask_irods/client.py
@@ -886,6 +886,23 @@ class IrodsPythonClient():
             # return buf
             return raw_out
 
+        # # EXAMPLE FOR IRULE: #METADATA RULE
+        # object_path = "/sdcCineca/home/httpadmin/tmp.txt"
+        # test_name = 'paolo2'
+        # inputs = {  # extra quotes for string literals
+        #     '*object': '"%s"' % object_path,
+        #     '*name': '"%s"' % test_name,
+        #     '*value': '"%s"' % test_name,
+        # }
+        # body = """
+        #     # add metadata
+        #     *attribute.*name = *value;
+        #     msiAssociateKeyValuePairsToObj(*attribute, *object, "-d")
+        # """
+        # output = imain.irule('test', body, inputs, 'ruleExecOut')
+        # print("TEST", output)
+        # # log.pp(output)
+
 # ####################################################
 # ####################################################
 # ####################################################
