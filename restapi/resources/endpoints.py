2cb2c14179cb18c382b7fa62f7b36e9bed76dde4
Mattia <m.dantonio81@gmail.com>
2018-08-05 17:00:01 +0200

    Added email sent to administrator on new registrations [now is sent by default, make it customizable?]

--- restapi/resources/endpoints.py
+++ restapi/resources/endpoints.py
@@ -620,6 +620,16 @@ class Profile(EndpointResource):
 
             send_activation_link(self.auth, user)
 
+            # Sending an email to the administrator
+            title = glom(
+                mem.customizer._configurations,
+                "project.title",
+                default='Unkown title')
+            subject = "%s New credentials requested" % title
+            body = "New credentials request from %s" % user.email
+
+            send_mail(body, subject)
+
             msg = "We are sending an email to your email address where " + \
                 "you will find the link to activate your account"
             return msg
