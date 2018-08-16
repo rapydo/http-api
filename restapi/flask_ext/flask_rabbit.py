3695b516f6be94b113ba2b1cb235a68f0adcfc7d
merret <buurman@dkrz.de>
2018-08-16 16:53:24 +0200

    Added log_json_to_queue method to RabbitMQ connection wrapper.

--- restapi/flask_ext/flask_rabbit.py
+++ restapi/flask_ext/flask_rabbit.py
@@ -34,27 +34,20 @@ class RabbitExt(BaseExtension):
 
 
         log.debug('Connecting to the Rabbit')
-        conn_wrapper = RabbitWrapper(self.variables, dont_connect)
+        conn_wrapper = RabbitWrapper(self.variables)
         log.debug('Connection wrapper was created, will be passed back.')
         return conn_wrapper
 
 class RabbitWrapper(object):
 
-    def __init__(self, variables, dont_connect=False):
+    def __init__(self, variables):
         log.debug('Creating RabbitMQ connection wrapper with variables %s' % variables)
         self.__variables = variables
         self.__connection = None
         self.__channel = None
-        self.__dont_connect = dont_connect
-        self.__couldnt_connect = 0
         # TODO: Declare queue and exchange, just in case?
         
         # Initial connection:
-        if self.__dont_connect:
-            log.warn('Will not connect to RabbitMQ (dont_connect = True).')
-            log.debug('Creating RabbitMQ connection wrapper... done. (without connection).')
-            return None
-
         try:
             self.__connect()
             log.debug('Creating RabbitMQ connection wrapper... done. (successful).')
@@ -84,7 +77,6 @@ class RabbitWrapper(object):
                     credentials = credentials
                 )
             )
-            self.__couldnt_connect = 0
             log.info('Connecting to the Rabbit... done.')
 
         except pika.exceptions.AMQPConnectionError as e:
@@ -93,8 +85,109 @@ class RabbitWrapper(object):
             '''
             log.warn('Connecting to the Rabbit... failed (%s)' % e)
             self.__connection = None
-            self.__couldnt_connect = self.__couldnt_connect+1
             raise e
+
+
+
+    '''
+    Send a log message to the RabbitMQ queue, unless
+    the dont-connect parameter is set. In that case,
+    the messages get logged into the normal log files.
+    If the connection is dead, reconnection is attempted,
+    but not eternally.
+    :param dictionary_message: JSON log message
+    :param app_name: App name (will be used for the ElasticSearch index name)
+    :param exchange: RabbitMQ exchange where the message should be sent
+    :param queue: RabbitMQ routing key.
+    '''
+    def log_json_to_queue(self, dictionary_message, app_name, exchange, queue):
+        log.verbose('Asked to log (%s, %s, %s): %s' % (exchange, queue, app_name, dictionary_message))
+        body = json.dumps(dictionary_message)
+
+        # Settings for the message:
+        filter_code = 'de.dkrz.seadata.filter_code.json'
+        permanent_delivery=2
+        props = pika.BasicProperties(
+            delivery_mode=permanent_delivery,
+            headers={'app_name': app_name, 'filter_code': filter_code},
+        )
+
+        # Try sending n times:
+        success = False
+        max_publish = 3
+        e = None
+        for i in range(max_publish):
+            log.verbose('Trying to send message to RabbitMQ in try (%s/%s)' % ((i+1), max_publish))
+
+            try:
+                
+                if self.__connection is None:
+                    self.__connect()
+                elif not self.__connection.is_open:
+                    self.__connect()
+            
+                channel = self.__get_channel()
+                success = channel.basic_publish(
+                    exchange=exchange,
+                    routing_key=queue,
+                    properties=props,
+                    body=body,
+                    mandatory=True
+                )
+                if success:
+                    log.verbose('Succeeded to send message to RabbitMQ in try (%s/%s)' % ((i+1), max_publish))
+                    break
+                else:
+                    log.warn('Log fail without clear reason.')
+                
+            except pika.exceptions.ConnectionClosed as e:
+                # TODO: This happens often. Check if heartbeat solves problem.
+                log.info('Failed to send log message in try (%s/%s), because connection is dead (%s).'
+                    % ((i+1), max_publish, e))
+                self.__connection = None
+                continue
+
+            except pika.exceptions.AMQPConnectionError as e:
+                log.info('Failed to send log message in try (%s/%s) because connection failed (%s).'
+                    % ((i+1), max_publish, e))
+                self.__connection = None
+                continue
+
+            except pika.exceptions.AMQPChannelError as e:
+                log.info('Failed to send log message in try (%s/%s), because channel is dead (%s).'
+                    % ((i+1), max_publish, e))
+                self.__channel = None
+                continue
+
+            except AttributeError as e:
+                log.info('Failed to send log message in try (%s/%s) (%s).' % ((i+1), max_publish, e))
+                self.__connection = None
+                continue
+
+            # If failed each time:
+            if i+1 >= max_publish:
+                log.warning('Could not log to RabbitMQ (%s), logging here instead...' % e)
+                log.info('RABBIT LOG MESSAGE (%s, %s, %s): %s' % (app_name, exchange, queue, body))
+                break
+
+
+    '''
+    Return existing channel (if healthy) or create and
+    return new one.
+    :return: The channel, or None if connection is switched off.
+    :raises: AttributeError if the connection is None.
+    '''
+    def __get_channel(self):
+
+        if self.__channel is None:
+            log.verbose('Creating new channel.')
+            self.__channel = self.__connection.channel()
+
+        elif self.__channel.is_closed or self.__channel.is_closing:
+            log.verbose('Recreating channel.')
+            self.__channel = self.__connection.channel()
+        
+        return self.__channel
 
 
     '''
@@ -102,8 +195,6 @@ class RabbitWrapper(object):
     '''
     def close_connection(self):
         # TODO: This must be called!
-        if self.__dont_connect:
-            return
         if self.__connection.is_closed or self.__connection.is_closing:
             log.debug('Connection already closed or closing.')
         else:
