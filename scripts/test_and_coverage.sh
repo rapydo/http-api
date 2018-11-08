472cd0474b2d7961d9ad45b7290f45257391ab07
Mattia <m.dantonio81@gmail.com>
2018-11-08 11:45:11 +0100

    [rapydo/issues#125] Added restapi wait to replace the sleep 40 removed with previous commits

--- scripts/test_and_coverage.sh
+++ scripts/test_and_coverage.sh
@@ -100,6 +100,7 @@ if [ "$PROJECT" != "COVERAGE" ]; then
 
 	rapydo --development --project ${PROJECT} shell backend --command 'restapi --help'
 	# Beware!! Cleaning DB before starting the tests
+	rapydo --development --project ${PROJECT} shell backend --command 'restapi wait'
 	rapydo --development --project ${PROJECT} shell backend --command 'restapi forced-clean'
 
 	# Test API and calculate coverage
