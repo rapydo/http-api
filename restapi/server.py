dbf355d8f80bbe3be8879d6623dde9bd7ef5ce4e
Mattia <m.dantonio81@gmail.com>
2019-02-12 08:08:39 +0100

    [rapydo/issues#117] Check on smtp configuration at startup

--- restapi/server.py
+++ restapi/server.py
@@ -17,7 +17,7 @@ from restapi.customization import Customizer
 from restapi.confs import PRODUCTION
 from restapi.protocols.restful import Api, farmer, create_endpoints
 from restapi.services.detect import detector
-from restapi.services.mail import send_mail_is_active
+from restapi.services.mail import send_mail_is_active, test_smtp_client
 from utilities.globals import mem
 from utilities.logs import \
     get_logger, \
@@ -286,8 +286,8 @@ def create_app(name=__name__,
 
         return response
 
-    if send_mail_is_active:
-        log.warning("Found SMTP configuration, should be checked")
+    if send_mail_is_active and not test_smtp_client():
+        log.exit("Bad SMTP configuration, unable to create a client")
     ##############################
     # and the flask App is ready now:
     log.info("Boot completed")
