701a90c294674946373b1eb17867829dbb1a3598
Mattia <m.dantonio81@gmail.com>
2019-03-05 18:14:30 +0100

    Restored restapi help during tests

--- scripts/test_and_coverage.sh
+++ scripts/test_and_coverage.sh
@@ -86,7 +86,7 @@ if [ "$PROJECT" != "COVERAGE" ]; then
 	rapydo --development --project ${PROJECT} start
 	docker ps -a
 
-	# rapydo --development --project ${PROJECT} shell backend --command 'restapi --help'
+	rapydo --development --project ${PROJECT} shell backend --command 'restapi --help'
 	# Beware!! Cleaning DB before starting the tests
 	rapydo --development --project ${PROJECT} shell backend --command 'restapi wait'
 	rapydo --development --project ${PROJECT} shell backend --command 'restapi forced-clean'
