3034aad7280c45ab6413e222d21a4443ef93effa
Mattia <m.dantonio81@gmail.com>
2019-03-13 08:07:18 +0100

    Debug code

--- restapi/rest/definition.py
+++ restapi/rest/definition.py
@@ -323,6 +323,8 @@ class EndpointResource(Resource):
             defined_content = ""
             errors = None
 
+        log.critical("%s %s %s %s", defined_content, errors, code, head_method)
+
         return self.force_response(
             defined_content=defined_content,
             errors=errors,
