9ab3c9d1af4507f48040a5b33ac01568373506fb
Mattia <m.dantonio81@gmail.com>
2019-07-12 10:26:56 +0200

    Celery beat app

---
+++ restapi/flask_ext/flask_celery/beat.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+Celery pattern. Some interesting read here:
+
+http://blog.miguelgrinberg.com/post/celery-and-the-flask-application-factory-pattern
+
+Of course that discussion is not enough for
+a flask templating framework like ours.
+So we made some improvement along the code.
+
+"""
+
+# from restapi.server import create_app
+from utilities import CUSTOM_PACKAGE
+from utilities.meta import Meta
+from utilities.logs import get_logger
+from restapi.services.detect import detector
+
+from flask import Flask
+
+log = get_logger(__name__)
+
+################################################
+# Reload Flask app code also for the worker
+# This is necessary to have the app context available
+# app = create_app(worker_mode=True)
+app = Flask("beat")
+
+app.extensions = detector.init_services(
+    app=app, worker_mode=True,
+    project_init=False, project_clean=False
+)
+
+celery_app = app.extensions.get('celery').celery_app
+celery_app.app = app
+
+
+def get_service(service, **kwargs):
+    return celery_app.app.extensions.get(service).get_instance(**kwargs)
+
+
+celery_app.get_service = get_service
+
+################################################
+# Import tasks modules to make sure all tasks are available
+
+# meta = Meta()
+# submodules = meta.import_submodules_from_package("%s.tasks" % CUSTOM_PACKAGE)
+
+log.debug("Celery beat is ready %s", celery_app)
