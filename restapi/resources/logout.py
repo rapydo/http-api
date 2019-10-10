072b3a4eeffc6da0fd4fc754973028d881a76480
Mattia <m.dantonio81@gmail.com>
2019-10-10 11:02:51 +0200

    Logout endpoint is now based on new endpoint definition

--- restapi/resources/logout.py
+++ restapi/resources/logout.py
@@ -5,6 +5,29 @@ from restapi.rest.definition import EndpointResource
 
 class Logout(EndpointResource):
     """ Let the logged user escape from here, invalidating current token """
+    baseuri = "/auth"
+    labels = ["authentication"]
+
+    SPECS = {
+        "mapping": {
+            "tologout": "/logout"
+        }
+    }
+
+    GET = {
+        "tologout": {
+            "summary": "Logout from current credentials",
+            "description": "Invalidate current registered token",
+            "custom": {
+                "authentication": True
+            },
+            "responses": {
+                "200": {
+                    "description": "Token correctly removed"
+                }
+            }
+        }
+    }
 
     def get(self):
         self.auth.invalidate_token(token=self.auth.get_token())
