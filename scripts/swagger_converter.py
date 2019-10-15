e245d7d2565b6cf07561ebf0c037d4b69c62262d
Mattia <m.dantonio81@gmail.com>
2019-10-15 10:30:44 +0200

    Added utility script to convert swaggers yaml to new endpoint-based definitions

---
+++ scripts/swagger_converter.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import yaml
+import json
+
+from utilities.configuration import mix
+from utilities.logs import get_logger
+
+log = get_logger(__name__)
+
+if len(sys.argv) <= 1:
+    log.exit("Usage: %s project_name", sys.argv[0])
+
+PROJECT = sys.argv[1]
+PROJECT_DIR = "projects/%s/backend/swagger" % PROJECT
+
+if not os.path.exists(PROJECT_DIR):
+    log.exit("%s folder does not exist", PROJECT_DIR)
+
+if not os.path.isdir(PROJECT_DIR):
+    log.exit("%s is not a folder", PROJECT_DIR)
+
+yamls = ["specs", "get", "post", "put", "patch", "delete"]
+for swagger_folder in os.listdir(PROJECT_DIR):
+
+    conf_output = ""
+    decorators_output = ""
+    pfile = None
+    pclass = None
+    mappings = None
+    for y in yamls:
+        f = os.path.join(PROJECT_DIR, swagger_folder, y + ".yaml")
+
+        if not os.path.isfile(f):
+            continue
+
+        with open(f, 'r') as yaml_in:
+            yaml_object = yaml.safe_load(yaml_in)
+            j = json.loads(json.dumps(yaml_object))
+
+            if y == 'specs':
+                pfile = j.pop('file')
+                pclass = j.pop('class')
+                schema = j.pop('schema')
+                if schema is not None:
+                    schema = schema.get('expose')
+                labels = j.pop('labels', [])
+                mappings = j.pop("mapping")
+
+                if len(j) > 0:
+                    log.exit("Found unexpected key: %s", j)
+
+                if schema:
+                    conf_output += "\nschema_expose = True"
+                conf_output += "\nlabels = %s" % labels
+            else:
+                common = j.pop('common', {})
+                # log.critical(common)
+                keys = set(j.keys())
+                data = {}
+                for m in keys:
+                    if m not in mappings:
+                        log.exit("Missing %s label in %f.%p.specs", m, pfile, pclass)
+
+                    u = mappings.get(m)
+
+                    conf = mix(common, j.pop(m))
+
+                    if 'custom' in conf:
+                        auth = conf['custom'].pop('authentication', False)
+                        roles = conf['custom'].pop('authorized', None)
+                        if auth:
+                            if roles is None:
+                                decorators_output += "\n@authentication.required()"
+                                decorators_output += "\ndef %s(self...\n" % y
+                            else:
+                                decorators_output += "\n@authentication.required(roles=%s)" % roles
+                                decorators_output += "\ndef %s(self...\n" % y
+                    data[u] = conf
+
+                if len(j) > 0:
+                    log.exit("Found unexpected key: %s", j)
+                conf_output += "\n%s = %s" % (y.upper(), data)
+
+    print("***************************************")
+    print("# Conf in %s.%s (%s)" % (pfile, pclass, swagger_folder))
+    print(conf_output)
+    if len(decorators_output) > 0:
+        print("\nfrom restapi.protocols.bearer import authentication")
+    print(decorators_output)
