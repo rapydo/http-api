ecd8d199f76e1119c0c9e9e88d2b7b8fb864de80
Mattia <m.dantonio81@gmail.com>
2020-02-12 09:12:30 +0100

    Undo previous commit

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         "celery",
         "flower==0.9.3",
         "celery-redbeat==0.13.0",
-        "celerybeat-mongo @ https://github.com/zmap/celerybeat-mongo.git",
+        "celerybeat-mongo @ https://github.com/zmap/celerybeat-mongo.git@master",
         "redis",
         "elasticsearch",
         "Flask-Migrate",
