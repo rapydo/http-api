d0ff5fd52f60eda794b4b5e3b7fd84d8211b21ef
Mattia <m.dantonio81@gmail.com>
2020-02-12 09:07:24 +0100

    https://github.com/zmap/celerybeat-mongo.git@master -> https://github.com/zmap/celerybeat-mongo.git

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         "celery",
         "flower==0.9.3",
         "celery-redbeat==0.13.0",
-        "celerybeat-mongo @ https://github.com/zmap/celerybeat-mongo.git@master",
+        "celerybeat-mongo @ https://github.com/zmap/celerybeat-mongo.git",
         "redis",
         "elasticsearch",
         "Flask-Migrate",
