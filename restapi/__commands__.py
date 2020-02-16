9c81d1579bade1f5b2898d34bae6ea75a05d6e0a
Mattia <m.dantonio81@gmail.com>
2020-02-16 08:22:04 +0100

    Fixed name redefinition from outer scope

--- restapi/__commands__.py
+++ restapi/__commands__.py
@@ -30,24 +30,11 @@ def main(args, another_app=None):
         if current_app is None or current_app.strip() == '':
             os.environ[APP] = '{}.__main__'.format(current_package)
 
-    cli = FlaskGroup()
+    fg_cli = FlaskGroup()
     options = {'prog_name': 'restapi', 'args': args}
 
     # cannot catch for CTRL+c
-    cli.main(**options)
-
-    # try:
-    #     cli.main(**options)
-    # except SystemExit as e:
-    #     if str(e) == "3":
-    #         print("AH!")
-    #     else:
-    #         # it looks like there is no Keyboard interrupt with flask
-    #         log.warning("Flask received: system exit")
-    # except BaseException as e:
-    #     # do not let flask close the application
-    #     # so we can do more code after closing
-    #     log.error(e)
+    fg_cli.main(**options)
 
 
 def flask_cli(options=None):
