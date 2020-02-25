6c5095f198f12a8ea695222b9f1017246b8e1b66
Mattia <m.dantonio81@gmail.com>
2020-02-25 10:06:51 +0100

    Tests with marshal_with integration

--- restapi/resources/apispecs.py
+++ restapi/resources/apispecs.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from restapi.rest.definition import EndpointResource
+from restapi.exceptions import RestApiException
 from restapi import decorators as decorate
 
 from flask_apispec import use_kwargs, marshal_with
@@ -16,8 +17,25 @@ class UserSchema(Schema):
     created_at = fields.DateTime(required=True)
 
 
+class Wrapper(Schema):
+    data: dict()
+
+
+class Error(Schema):
+    error: fields.Str()
+
+
+class old_responses(Schema):
+    defined_content = fields.Str()
+    errors = fields.Str()
+
+
 class OutSchema(Schema):
     value = fields.Int()
+
+
+class OutSchema1(Schema):
+    value = fields.Str()
 
 
 class ApiSpecsPoC(MethodResource, EndpointResource):
@@ -40,10 +58,22 @@ class ApiSpecsPoC(MethodResource, EndpointResource):
     # def get(self, species):
     # Example3
     @use_kwargs(UserSchema)
-    @marshal_with(OutSchema)
+    @marshal_with(Error, code=400)
+    @marshal_with(Error, code=404)
+    @marshal_with(old_responses, code=200)
+    # @marshal_with(OutSchema, code=200)
+    @marshal_with(OutSchema1, code=201)
+    @marshal_with(None, code=204)
     @decorate.catch_error()
     def get(self, **kwargs):
 
         log.critical(kwargs)
 
-        return {"value": '10'}
+        # return self.force_response("blabla")
+        data = {"mydata": "123", "xyz": "abc"}
+        errors = ["x", "y"]
+        return self.force_response(data, errors=errors)
+        raise RestApiException("Just an error")
+
+        return {"value": '10'}, 200
+        # return {"value": '10'}, 201
