b62e42f2d4020831f9694f10df2cd7dfb905fff7
Mattia <m.dantonio81@gmail.com>
2020-03-31 06:36:37 +0200

    Bug fix

--- restapi/models/sqlalchemy.py
+++ restapi/models/sqlalchemy.py
@@ -10,7 +10,7 @@ db = OriginalAlchemy()
 # Define multi-multi relation
 roles_users = db.Table(
     'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
     db.Column('role_id', db.Integer(), db.ForeignKey('role.id')),
 )
 
