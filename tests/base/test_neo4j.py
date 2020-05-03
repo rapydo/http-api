64e1caaf578a841ec48a90b27e7da3eced2629ec
Mattia <m.dantonio81@gmail.com>
2020-05-03 19:39:26 +0200

    Added tests on neo4j fuzzytokenizer, sanitize input and cypher query

--- tests/base/test_neo4j.py
+++ tests/base/test_neo4j.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
 from restapi.services.detect import detector
 from restapi.utilities.logs import log
 
@@ -12,5 +11,21 @@ def test_neo4j():
         return False
 
     neo4j = detector.connectors_instances.get('neo4j').get_instance()
+    for u in neo4j.cypher("MATCH (u: User) RETURN u limit 1"):
+        u.inflate(neo4j.User)
+        assert u.email is not None
+        break
 
-    return True
+    assert neo4j.createUniqueIndex('a', 'b') == 'a#_#b'
+
+    assert neo4j.sanitize_input("x") == "x"
+    assert neo4j.sanitize_input("x ") == "x"
+    assert neo4j.sanitize_input(" x") == "x"
+    assert neo4j.sanitize_input("*x") == "x"
+    assert neo4j.sanitize_input("x*") == "x"
+    assert neo4j.sanitize_input("x~") == "x"
+    assert neo4j.sanitize_input("~x") == "x"
+    assert neo4j.sanitize_input("x'") == "x\\'"
+    assert neo4j.sanitize_input("   *~ ~** x  ~~**  ") == "x"
+
+    assert neo4j.fuzzy_tokenize("x AND y") == "x~1 AND y~1"
