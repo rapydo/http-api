d0552c510dd9e5acc88bbab6b4314f25e30e7770
Mattia <m.dantonio81@gmail.com>
2020-05-10 09:29:02 +0200

    Tests on celery beat

--- tests/base/test_celery.py
+++ tests/base/test_celery.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from restapi.connectors.celery import CeleryExt
 from restapi.services.detect import detector
 from restapi.utilities.logs import log
 
@@ -10,9 +11,16 @@ def test_celery():
         log.warning("Skipping celery test: service not available")
         return False
 
-    celery = detector.connectors_instances.get('celery').get_instance()
+    detector.connectors_instances.get('celery').get_instance()
 
-    if celery.CELERYBEAT_SCHEDULER is None:
+    if CeleryExt.CELERYBEAT_SCHEDULER is None:
         log.warning("Skipping celery beat tests: service not available")
     else:
-        assert celery.get_periodic_task('does_not_exist') is None
+        assert CeleryExt.get_periodic_task('does_not_exist') is None
+        assert not CeleryExt.delete_periodic_task('does_not_exist')
+
+        CeleryExt.create_periodic_task(
+            name='does_not_exist',
+            task='task.does.not.exists',
+            every='60'
+        )
