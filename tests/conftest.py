386ddad659cd9dd9a6f88831ca8b2c86ddfd74df
Mattia <m.dantonio81@gmail.com>
2020-05-11 10:56:12 +0200

    Bug fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ from restapi.server import create_app
 
 @pytest.fixture
 def app():
-    os.environ['TESTING'] = 1
+    os.environ['TESTING'] = "1"
     app = create_app(testing_mode=True)
     return app
