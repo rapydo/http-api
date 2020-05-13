6e29a0cbb53ffa0eeb70a6d2820acce291dfdf92
Mattia <m.dantonio81@gmail.com>
2020-05-13 16:44:05 +0200

    Bug fix

--- tests/base/test_endpoints_registration_and_activation.py
+++ tests/base/test_endpoints_registration_and_activation.py
@@ -18,40 +18,39 @@ class TestApp(BaseTests):
         assert self.get_content(r) == 'Empty input'
 
         # registration, missing information
-        data = {'x': 'y'}
-        r = client.post(AUTH_URI + '/profile', data=data)
+        r = client.post(AUTH_URI + '/profile', data={'x': 'y'})
         assert r.status_code == 400
         assert self.get_content(r) == 'Missing input: password'
-        data = {}
-        data['password'] = self.randomString()
-        r = client.post(AUTH_URI + '/profile', data=data)
+        registration_data = {}
+        registration_data['password'] = self.randomString()
+        r = client.post(AUTH_URI + '/profile', data=registration_data)
         assert r.status_code == 400
         assert self.get_content(r) == 'Missing input: email'
-        data['email'] = BaseAuthentication.default_user
-        r = client.post(AUTH_URI + '/profile', data=data)
+        registration_data['email'] = BaseAuthentication.default_user
+        r = client.post(AUTH_URI + '/profile', data=registration_data)
         assert r.status_code == 400
         assert self.get_content(r) == 'Missing input: name'
-        data['name'] = 'Mr'
-        r = client.post(AUTH_URI + '/profile', data=data)
+        registration_data['name'] = 'Mr'
+        r = client.post(AUTH_URI + '/profile', data=registration_data)
         assert r.status_code == 400
         assert self.get_content(r) == 'Missing input: surname'
 
-        data['surname'] = 'Brown'
-        r = client.post(AUTH_URI + '/profile', data=data)
+        registration_data['surname'] = 'Brown'
+        r = client.post(AUTH_URI + '/profile', data=registration_data)
         assert r.status_code == 400
         m = "This user already exists: {}".format(BaseAuthentication.default_user)
         assert self.get_content(r) == m
 
-        data['email'] = 'mock@nomail.org'
-        r = client.post(AUTH_URI + '/profile', data=data)
+        registration_data['email'] = 'mock@nomail.org'
+        r = client.post(AUTH_URI + '/profile', data=registration_data)
         # now the user is created but INACTIVE, activation endpoint is needed
         assert r.status_code == 200
 
         # This will fail because the user is not active
         self.do_login(
             client,
-            data['email'],
-            data['password'],
+            registration_data['email'],
+            registration_data['password'],
             status_code=403,
             # error='Sorry, this account is not active'
         )
@@ -95,7 +94,9 @@ class TestApp(BaseTests):
 
         # request activation, correct username
         r = client.post(
-            AUTH_URI + '/profile/activate', data={'username': data['email']})
+            AUTH_URI + '/profile/activate',
+            data={'username': registration_data['email']}
+        )
         # return is 200, but no token will be generated and no mail will be sent
         assert r.status_code == 200
         assert self.get_content(r) == activation_message
