d1c9cca22ee73fd5eea82180b5eaefee4854d84a
Mattia <m.dantonio81@gmail.com>
2020-05-26 07:59:57 +0200

    Added random locale to Faker initialization

--- tests/base/test_endpoints_admin_users.py
+++ tests/base/test_endpoints_admin_users.py
@@ -36,8 +36,6 @@ class TestApp(BaseTests):
 
         schema = self.getDynamicInputSchema(client, endpoint, headers)
         data = self.buildData(schema)
-        log.critical(data)
-        assert 0
 
         r = client.post(url, data=data, headers=headers)
         assert r.status_code == 200
