460de61572fc955880d61d43c33e6a449184cb39
Mattia <m.dantonio81@gmail.com>
2020-05-28 22:54:00 +0200

    Debug code

--- tests/base/test_schemathesis.py
+++ tests/base/test_schemathesis.py
@@ -2,13 +2,11 @@
 # import requests
 import os
 import schemathesis
-import urllib.parse
 from hypothesis import settings, HealthCheck
 import werkzeug
 import json
 
 from restapi.server import create_app
-from restapi.tests import BaseTests
 from restapi.services.authentication import BaseAuthentication
 from restapi.utilities.logs import log
 
@@ -16,21 +14,32 @@ from restapi.utilities.logs import log
 RUN_SCHEMATHESIS = os.getenv("RUN_SCHEMATHESIS", "1") == "1"
 
 
+def get_auth_token(client):
+    BaseAuthentication.load_default_user()
+    BaseAuthentication.load_roles()
+    USER = BaseAuthentication.default_user
+    PWD = BaseAuthentication.default_password
+    data = {'username': USER, 'password': PWD}
+
+    r = client.post('/auth/login', data=data)
+    assert r.status_code == 200
+    token = json.loads(r.data.decode('utf-8'))
+    assert token is not None
+
+    return token, {'Authorization': f'Bearer {token}'}
+
+
 if not RUN_SCHEMATHESIS:
     log.warning("Skipping schemathesis")
 else:
     app = create_app(testing_mode=True)
     client = werkzeug.Client(app, werkzeug.wrappers.Response)
-    BaseAuthentication.load_default_user()
-    BaseAuthentication.load_roles()
-    USER = BaseAuthentication.default_user
-    PWD = BaseAuthentication.default_password
-    token, auth_header = BaseTests.do_login(client, USER, PWD)
+    token, auth_header = get_auth_token(client)
 
     # it does not handle custom headers => the endpoint will provide partial schema
     # due to missing authentication => skipping all private endpoints and schemas
     # schema = schemathesis.from_wsgi('/api/swagger', app)
-    r = client.get(f'/api/swagger?access_token={urllib.parse.quote(token)}')
+    r = client.get(f'/api/swagger?access_token={token}')
     schema = json.loads(r.get_data().decode())
     schema = schemathesis.from_dict(schema, app=app)
 
