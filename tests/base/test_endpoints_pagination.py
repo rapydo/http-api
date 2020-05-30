5566c6588e645056d4e5c5070631fb337fe04bc5
Mattia <m.dantonio81@gmail.com>
2020-05-30 11:41:53 +0200

    Fixed range used in pagination tests

--- tests/base/test_endpoints_pagination.py
+++ tests/base/test_endpoints_pagination.py
@@ -35,19 +35,23 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 10
-        assert content[0] == 21
-        assert content[19] == 30
+        assert content[0] == 11
+        assert content[9] == 20
+
+        r = client.get(f"{API_URI}/tests/pagination", data={"page": 2, "size": 100})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 50
+        assert content[0] == 101
+        assert content[49] == 150
 
         r = client.get(f"{API_URI}/tests/pagination", data={"page": 20})
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 0
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"size": 200})
-        assert r.status_code == 200
-        content = self.get_content(r)
-        # Size is capped to 100
-        assert len(content) == 100
+        r = client.get(f"{API_URI}/tests/pagination", data={"size": 101})
+        assert r.status_code == 400
 
         r = client.get(f"{API_URI}/tests/pagination", data={"page": -5})
         assert r.status_code == 400
