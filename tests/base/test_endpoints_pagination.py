499116ae4c46ce71bc6a07bb9d3848cd5513d599
Mattia <m.dantonio81@gmail.com>
2020-05-30 16:00:51 +0200

    Tests on both get and post endpoints with pagiation

--- tests/base/test_endpoints_pagination.py
+++ tests/base/test_endpoints_pagination.py
@@ -6,7 +6,6 @@ class TestApp(BaseTests):
 
     def test_GET_specs(self, client):
 
-        # Old specs produced by swagger inputs - to be deprecated
         r = client.get(f"{API_URI}/tests/pagination")
         assert r.status_code == 200
         content = self.get_content(r)
@@ -14,8 +13,14 @@ class TestApp(BaseTests):
         assert content[0] == 1
         assert content[19] == 20
 
+        r = client.get(f"{API_URI}/tests/pagination", params={"get_total": True})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert content == 150
+
+        # Check precedence: get_total wins
         data = {"get_total": True, "page": 1, "size": 20}
-        r = client.get(f"{API_URI}/tests/pagination", data=data)
+        r = client.get(f"{API_URI}/tests/pagination", params=data)
         assert r.status_code == 200
         content = self.get_content(r)
         assert content == 150
@@ -24,40 +29,117 @@ class TestApp(BaseTests):
         content = self.get_content(r)
         assert content == 150
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": 2})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": 2})
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 20
         assert content[0] == 21
         assert content[19] == 40
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": 2, "size": 10})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": 2, "size": 10})
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 10
         assert content[0] == 11
         assert content[9] == 20
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": 2, "size": 100})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": 2, "size": 100})
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 50
         assert content[0] == 101
         assert content[49] == 150
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": 20})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": 20})
         assert r.status_code == 200
         content = self.get_content(r)
         assert len(content) == 0
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"size": 101})
+        r = client.get(f"{API_URI}/tests/pagination", params={"size": 101})
         assert r.status_code == 400
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": -5})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": -5})
         assert r.status_code == 400
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"size": -5})
+        r = client.get(f"{API_URI}/tests/pagination", params={"size": -5})
         assert r.status_code == 400
 
-        r = client.get(f"{API_URI}/tests/pagination", data={"page": -5, "size": -5})
+        r = client.get(f"{API_URI}/tests/pagination", params={"page": -5, "size": -5})
         assert r.status_code == 400
+
+        r = client.post(f"{API_URI}/tests/pagination")
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 20
+        assert content[0] == 1
+        assert content[19] == 20
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"get_total": True})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert content == 150
+
+        # Check precedence: get_total wins
+        data = {"get_total": True, "page": 1, "size": 20}
+        r = client.post(f"{API_URI}/tests/pagination", data=data)
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert content == 150
+
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert content == 150
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": 2})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 20
+        assert content[0] == 21
+        assert content[19] == 40
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": 2, "size": 10})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 10
+        assert content[0] == 11
+        assert content[9] == 20
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": 2, "size": 100})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 50
+        assert content[0] == 101
+        assert content[49] == 150
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": 20})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 0
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"size": 101})
+        assert r.status_code == 400
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": -5})
+        assert r.status_code == 400
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"size": -5})
+        assert r.status_code == 400
+
+        r = client.post(f"{API_URI}/tests/pagination", data={"page": -5, "size": -5})
+        assert r.status_code == 400
+
+        # Final check:
+        # get only accept query parameters
+        # post only accept body parameters
+
+        r = client.get(f"{API_URI}/tests/pagination", data={"get_total": True})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        # Request get_total as body parameter but is ignored => sent a list of elements
+        assert isinstance(content, list)
+
+        # Request get_total as query parameter but is ignored => sent a list of elements
+        r = client.post(f"{API_URI}/tests/pagination", params={"get_total": True})
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert isinstance(content, list)
