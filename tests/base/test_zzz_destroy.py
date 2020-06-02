645d4c1268fb9796f1dcc1a1985bd302808dbec3
Mattia <m.dantonio81@gmail.com>
2020-06-02 19:07:40 +0200

    Moved destroy test as last test, after schemathesis

--- tests/base/test_zzz_destroy.py
+++ tests/base/test_zzz_destroy.py
@@ -4,6 +4,7 @@ from restapi.utilities.logs import log
 
 def test_destroy():
 
+    # Only executed if tests are run with --destroy flag
     value = os.getenv("TEST_DESTROY_MODE", '0')
     if value == '1':
         create_app(destroy_mode=True)
