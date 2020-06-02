07412afed4eb3e192c09cebb030f37752005382d
Mattia <m.dantonio81@gmail.com>
2020-06-02 18:57:30 +0200

    Moved test destroy mode on a dedicated tests (executed as first test)

---
+++ tests/base/test_aaa_destroy.py
@@ -0,0 +1,15 @@
+import os
+from restapi.server import create_app
+from restapi.utilities.logs import log
+
+def test_destroy():
+
+    value = os.getenv("TEST_DESTROY_MODE", '0')
+    if value == '1':
+        create_app(destroy_mode=True)
+        create_app(init_mode=True)
+    else:
+        log.info(
+            "Skipping destroy test, TEST_DESTROY_MODE is {}",
+            value
+        )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 
 from restapi.server import create_app
@@ -7,6 +6,7 @@ from restapi.tests import get_faker
 
 @pytest.fixture
 def app():
+
     app = create_app(testing_mode=True)
     return app
 
@@ -14,8 +14,3 @@ def app():
 @pytest.fixture
 def fake():
     return get_faker()
-
-
-def pytest_sessionfinish(session, exitstatus):
-    if os.getenv("TEST_DESTROY_MODE", '0') == '1':
-        create_app(destroy_mode=True)
