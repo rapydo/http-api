57c36fa16eca776738939f43b8ac76449b100a81
Mattia <m.dantonio81@gmail.com>
2020-06-11 08:39:23 +0200

    Testing pytest-mock

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import pytest
 
 from restapi.server import create_app
+from restapi.services.mailmock import SMTP
 from restapi.tests import get_faker
 
 
 @pytest.fixture
-def app():
+def app(mocker):
 
+    mocker.patch("smtplib.SMTP", return_value=SMTP)
+    mocker.patch("smtplib.SMTP_SSL", return_value=SMTP)
     app = create_app(testing_mode=True)
     return app
 
