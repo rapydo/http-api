f2893a684e2a1e15a936ac632f4c8df97c2b8992
Mattia <m.dantonio81@gmail.com>
2020-06-13 08:04:20 +0200

    Merged PushpinExt and PushpinClient

--- restapi/connectors/pushpin/__init__.py
+++ restapi/connectors/pushpin/__init__.py
@@ -30,23 +30,16 @@ class PushpinExt(Connector):
         port = variables.get("port")
 
         control_uri = f"http://{host}:{port}"
-        pubctrl = GripPubControl({"control_uri": control_uri})
+        self.pubctrl = GripPubControl({"control_uri": control_uri})
 
-        client = PushpinClient(pubctrl)
-
-        is_active = client.publish_on_stream("admin", "Connection test", sync=True)
+        is_active = self.publish_on_stream("admin", "Connection test", sync=True)
 
         if not is_active:
             raise ServiceUnavailable(f"Pushpin unavailable on {control_uri}")
-        return client
+        return self
 
     def disconnect(self, **kwargs):
         return
-
-
-class PushpinClient:
-    def __init__(self, pub):
-        self.pub = pub
 
     @staticmethod
     def callback(result, message):
@@ -57,13 +50,13 @@ class PushpinClient:
 
     def publish_on_stream(self, channel, message, sync=False):
         if not sync:
-            self.pub.publish_http_stream(
-                channel, message, callback=PushpinClient.callback
+            self.pubctrl.publish_http_stream(
+                channel, message, callback=PushpinExt.callback
             )
             return True
 
         try:
-            self.pub.publish_http_stream(channel, message, blocking=True)
+            self.pubctrl.publish_http_stream(channel, message, blocking=True)
             log.debug("Message successfully published on pushpin")
             return True
         except BaseException as e:
@@ -74,11 +67,11 @@ class PushpinClient:
     def publish_on_socket(self, channel, message, sync=False):
         item = Item(WebSocketMessageFormat(message, binary=False))
         if not sync:
-            self.pub.publish(channel, item, callback=self.callback)
+            self.pubctrl.publish(channel, item, callback=self.callback)
             return True
 
         try:
-            self.pub.publish(channel, item, blocking=True)
+            self.pubctrl.publish(channel, item, blocking=True)
             log.debug("Message successfully published on pushpin")
             return True
         except BaseException as e:
