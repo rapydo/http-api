ed8e9ea71d7b1eb37343bbacf6508491fdacb82c
Mattia <m.dantonio81@gmail.com>
2020-07-30 23:11:25 +0200

    # Bug fix

--- restapi/exceptions.py
+++ restapi/exceptions.py
@@ -16,7 +16,7 @@ class RestApiException(Exception):
         if status_code is None:
             status_code = 404
 
-        super().__init__(str(exception))
+        super().__init__(exception)
         self.status_code = status_code
         self.is_warning = is_warning
 
