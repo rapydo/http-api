80bba4148d8c1482c59eaa15bfbf7968e64f99ce
Mattia <m.dantonio81@gmail.com>
2020-07-30 22:33:07 +0200

    # Tests fix

--- tests/base/test_module_miscellanous.py
+++ tests/base/test_module_miscellanous.py
@@ -458,7 +458,7 @@ class TestApp(BaseTests):
         assert len(r["unique_delimited_list"]) == 3
         assert r["unique_delimited_list"][0] == "a"
         assert r["unique_delimited_list"][1] == "b"
-        assert r["unique_delimited_list"][1] == "c"
+        assert r["unique_delimited_list"][2] == "c"
 
         try:
             schema.load({"unique_delimited_list": "a,b,b"})
@@ -473,11 +473,11 @@ class TestApp(BaseTests):
         assert len(r["unique_delimited_list"]) == 3
         assert r["unique_delimited_list"][0] == "a"
         assert r["unique_delimited_list"][1] == "b"
-        assert r["unique_delimited_list"][1] == " c"
+        assert r["unique_delimited_list"][2] == " c"
 
         r = schema.load({"unique_delimited_list": "a,b,c "})
         assert "unique_delimited_list" in r
         assert len(r["unique_delimited_list"]) == 3
         assert r["unique_delimited_list"][0] == "a"
         assert r["unique_delimited_list"][1] == "b"
-        assert r["unique_delimited_list"][1] == "c "
+        assert r["unique_delimited_list"][2] == "c "
