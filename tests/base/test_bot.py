1040a93a747c8020c27180b172d67c44fae917da
Mattia <m.dantonio81@gmail.com>
2020-08-05 14:43:35 +0200

    # tests authorized bot requests /6

--- tests/base/test_bot.py
+++ tests/base/test_bot.py
@@ -1,7 +1,7 @@
+import asyncio
 import re
 from time import sleep
 
-import pytest
 from click.testing import CliRunner
 from telethon import TelegramClient
 from telethon.sessions import StringSession
@@ -17,15 +17,7 @@ session_str = Env.get("TELETHON_SESSION")
 botname = Env.get("TELEGRAM_BOTNAME")
 
 
-async def send_command(client, command):
-    await client.send_message(botname, command)
-    sleep(1)
-    messages = await client.get_messages(botname)
-    return messages[0].message
-
-
-@pytest.mark.asyncio
-async def test_bot():
+def test_bot():
 
     if not Env.get_bool("TELEGRAM_ENABLE"):
         log.warning("Skipping BOT tests: service not available")
@@ -34,54 +26,63 @@ async def test_bot():
     runner = CliRunner()
     runner.invoke(cli.bot, [])
 
-    client = TelegramClient(StringSession(session_str), api_id, api_hash)
-    await client.start()
-
-    # ############################# #
-    #           TEST ADMIN          #
-    # ############################# #
-
-    message = await send_command(client, "/me")
-    assert re.match(r"^Hello .*, your Telegram ID is [0-9]+", message)
-
-    message = await send_command(client, "/help")
-    assert "Available Commands:" in message
-    assert "- /help print this help" in message
-    assert "- /me info about yourself" in message
-    assert "- /status get server status" in message
-    assert "- /monitor get server monitoring stats" in message
-
-    # commands requiring APIs can only be tested in PRODUCTION MODE
-    if PRODUCTION:
-        message = await send_command(client, "/status")
-        assert message == "Server is alive"
-
-        message = await send_command(client, "/monitor")
-        assert message == '{"param": "Missing data for required field."}'
-
-        message = await send_command(client, "/monitor x")
-        assert message == '{"x": "Must be one of: disk, cpu, ram."}'
-
-        message = await send_command(client, "/monitor disk")
-        error = "Missing credentials in headers, e.g. Authorization: 'Bearer TOKEN'"
-        assert message == error
-
     from restapi.services.telegram import bot
 
-    # ############################# #
-    #          TEST USER            #
-    # ############################# #
-    bot.users = bot.admins
-    bot.admins = ["1234"]
-    # message = await send_command(client, "/me")
+    async def send_command(client, command):
+        await client.send_message(botname, command)
+        sleep(1)
+        messages = await client.get_messages(botname)
+        return messages[0].message
 
-    # ############################# #
-    #        TEST UNAUTHORIZED      #
-    # ############################# #
-    bot.admins = ["1234"]
-    bot.users = ["1234"]
-    message = await send_command(client, "/help")
-    error = "Permission denied, you are not authorized to execute this command"
-    assert message == error
+    async def test_commands():
+        client = TelegramClient(StringSession(session_str), api_id, api_hash)
+        await client.start()
+
+        # ############################# #
+        #           TEST ADMIN          #
+        # ############################# #
+
+        message = await send_command(client, "/me")
+        assert re.match(r"^Hello .*, your Telegram ID is [0-9]+", message)
+
+        message = await send_command(client, "/help")
+        assert "Available Commands:" in message
+        assert "- /help print this help" in message
+        assert "- /me info about yourself" in message
+        assert "- /status get server status" in message
+        assert "- /monitor get server monitoring stats" in message
+
+        # commands requiring APIs can only be tested in PRODUCTION MODE
+        if PRODUCTION:
+            message = await send_command(client, "/status")
+            assert message == "Server is alive"
+
+            message = await send_command(client, "/monitor")
+            assert message == '{"param": "Missing data for required field."}'
+
+            message = await send_command(client, "/monitor x")
+            assert message == '{"x": "Must be one of: disk, cpu, ram."}'
+
+            message = await send_command(client, "/monitor disk")
+            error = "Missing credentials in headers, e.g. Authorization: 'Bearer TOKEN'"
+            assert message == error
+
+        # ############################# #
+        #          TEST USER            #
+        # ############################# #
+        bot.users = bot.admins
+        bot.admins = ["1234"]
+        # message = await send_command(client, "/me")
+
+        # ############################# #
+        #        TEST UNAUTHORIZED      #
+        # ############################# #
+        bot.admins = ["1234"]
+        bot.users = ["1234"]
+        message = await send_command(client, "/help")
+        error = "Permission denied, you are not authorized to execute this command"
+        assert message == error
+
+    asyncio.run(test_commands())
 
     bot.shutdown()
