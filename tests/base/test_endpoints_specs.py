6a068175e87522ec9a5fdaa4a6c822ced2813b6a
Mattia <m.dantonio81@gmail.com>
2020-08-18 12:18:29 +0200

    # Tests fix

--- tests/base/test_endpoints_specs.py
+++ tests/base/test_endpoints_specs.py
@@ -6,33 +6,11 @@ from restapi.tests import API_URI, BaseTests
 class TestApp(BaseTests):
     def test_GET_specs(self, client):
 
-        # Old specs produced by swagger inputs - to be deprecated
-        r = client.get(f"{API_URI}/specs")
-        assert r.status_code == 200
-        content = self.get_content(r)
-        assert "basePath" in content
-        assert "consumes" in content
-        assert "produces" in content
-        assert "application/json" in content["consumes"]
-        assert "application/json" in content["produces"]
-        assert "host" in content
-        assert "info" in content
-        assert "schemes" in content
-        assert "swagger" in content
-        assert "tags" in content
-        assert "security" in content
-        assert "Bearer" in content["security"][0]
-        assert "securityDefinitions" in content
-        assert "Bearer" in content["securityDefinitions"]
-        assert "paths" in content
-        assert "/auth/login" in content["paths"]
-        assert "get" not in content["paths"]["/auth/login"]
-        assert "post" in content["paths"]["/auth/login"]
-        assert "put" not in content["paths"]["/auth/login"]
-        assert "delete" not in content["paths"]["/auth/login"]
-
-        # New spec produced by FlaskApiSpec
+        # Alias for /api/specs
         r = client.get(f"{API_URI}/swagger")
+        assert r.status_code == 200
+
+        r = client.get(f"{API_URI}/specs")
         assert r.status_code == 200
         content = self.get_content(r)
         assert "host" in content
