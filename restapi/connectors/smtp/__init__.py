36dc7d87911eb90cb335f45931dd38fa34d8457a
Mattia <m.dantonio81@gmail.com>
2020-10-29 12:08:52 +0100

    # Debug code [skip ci]

--- restapi/connectors/smtp/__init__.py
+++ restapi/connectors/smtp/__init__.py
@@ -23,6 +23,7 @@ class Mail(Connector):
         return (socket.gaierror, SMTPAuthenticationError)
 
     def connect(self, **kwargs):
+        log.warning("Debug SMTP: connect")
         self.variables = self.variables.copy()
         self.variables.update(kwargs)
 
@@ -53,6 +54,7 @@ class Mail(Connector):
         return self
 
     def disconnect(self):
+        log.warning("Debug SMTP: disconnect")
         self.disconnected = True
         self.smtp.quit()
         self.smtp = None
@@ -67,6 +69,7 @@ class Mail(Connector):
         bcc=None,
         plain_body=None,
     ):
+        log.warning("Debug SMTP: send")
 
         if not to_address:
             to_address = self.variables.get("admin")
