96c9cc9a84a33e98e6e932f173e4c725b76f2d36
Mattia <m.dantonio81@gmail.com>
2020-11-05 19:36:20 +0100

    # Bug fix

--- restapi/utilities/globals.py
+++ restapi/utilities/globals.py
@@ -2,16 +2,14 @@
 If you need things globally, come here and take.
 
 """
-from restapi.customizer import BaseCustomizer
+from typing import Any
 
 
 class mem:
 
-    customizer: BaseCustomizer
+    customizer: Any
     """
     Source:
     https://pythonconquerstheuniverse.wordpress.com/
         2010/10/20/a-globals-class-pattern-for-python/
     """
-
-    pass
