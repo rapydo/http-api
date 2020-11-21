b5a27281e29f7a08713fed3e060bf0b7213f50fb
Mattia <m.dantonio81@gmail.com>
2020-11-21 09:58:44 +0100

    # Tests fix /3

--- tests/create_template.sh
+++ tests/create_template.sh
@@ -2,6 +2,7 @@
 set -e
 
 TEMPLATE=$1
+AUTH=$2
 
 if [[ "$TEMPLATE" == "postgres" ]]; then
   # TOTP will be checked and disabled because AUTH_FORCE_FIRST_PASSWORD_CHANGE is False;
@@ -18,20 +19,20 @@ elif [[ "$TEMPLATE" == "mongo" ]]; then
 
 
 elif [[ "$TEMPLATE" == "celery-rabbit-rabbit" ]]; then
-  rapydo create prj -s celery -s rabbit --auth ${RANDOM_AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit --auth ${AUTH} --frontend no;
 elif [[ "$TEMPLATE" == "celery-rabbit-redis" ]]; then
-  rapydo create prj -s celery -s rabbit -s redis --auth ${RANDOM_AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit -s redis --auth ${AUTH} --frontend no;
 elif [[ "$TEMPLATE" == "celery-rabbit-mongo" ]]; then
-  rapydo create prj -s celery -s rabbit -s mongo --auth ${RANDOM_AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit -s mongo --auth ${AUTH} --frontend no;
 elif [[ "$TEMPLATE" == "celery-redis-redis" ]]; then
-  rapydo create prj -s celery -s redis --auth ${RANDOM_AUTH} --frontend no;
+  rapydo create prj -s celery -s redis --auth ${AUTH} --frontend no;
 elif [[ "$TEMPLATE" == "celery-redis-mongo" ]]; then
-  rapydo create prj -s celery -s redis -s mongo --auth ${RANDOM_AUTH} --frontend no;
+  rapydo create prj -s celery -s redis -s mongo --auth ${AUTH} --frontend no;
 
 
 elif [[ "$TEMPLATE" == "extra" ]]; then
-  rapydo create prjbase --auth ${RANDOM_AUTH} --frontend no;
-  rapydo create prj --current --extend prjbase -s ftp -s pushpin -s bot --auth ${RANDOM_AUTH} --frontend no --add-optionals -e AUTH_FORCE_FIRST_PASSWORD_CHANGE=true -e AUTH_MAX_PASSWORD_VALIDITY=10 -e AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER=30 -e AUTH_REGISTER_FAILED_LOGIN=True -e AUTH_MAX_LOGIN_ATTEMPTS=5 -e AUTH_SECOND_FACTOR_AUTHENTICATION=TOTP;
+  rapydo create prjbase --auth ${AUTH} --frontend no;
+  rapydo create prj --current --extend prjbase -s ftp -s pushpin -s bot --auth ${AUTH} --frontend no --add-optionals -e AUTH_FORCE_FIRST_PASSWORD_CHANGE=true -e AUTH_MAX_PASSWORD_VALIDITY=10 -e AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER=30 -e AUTH_REGISTER_FAILED_LOGIN=True -e AUTH_MAX_LOGIN_ATTEMPTS=5 -e AUTH_SECOND_FACTOR_AUTHENTICATION=TOTP;
 
 
 else
