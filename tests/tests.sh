05ca5fd60da020a3f5c75f279011e0694dbd2a65
Mattia <m.dantonio81@gmail.com>
2020-11-25 20:02:16 +0100

    Extended tests script to support coverage on projects

--- tests/tests.sh
+++ tests/tests.sh
@@ -8,19 +8,23 @@ fi
 
 export COVERAGE_FILE="/tmp/.coverage"
 
-
-CURRENT_PACKAGE="restapi"
-COV="--cov=$CURRENT_PACKAGE"
-
-echo "Launching unittests with coverage"
-echo "Package: $CURRENT_PACKAGE"
-sleep 1
-
 if [ -z "$2" ]; then
     folder=tests
 else
     folder=tests/$2
 fi
+
+
+if [ -z "$3" ]; then
+    CURRENT_PACKAGE="restapi"
+else
+    CURRENT_PACKAGE=$3
+
+COV="--cov=$CURRENT_PACKAGE"
+
+echo "Launching unittests with coverage"
+echo "Package: $CURRENT_PACKAGE"
+sleep 1
 
 # --timeout is powered by pytest-timeout
 py.test --confcutdir=tests --timeout=300 -x -s --cov-report=xml $COV $folder
