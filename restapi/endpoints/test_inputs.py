11015bf583a7dce33e3edd2f1df154b8ad46d2c0
Mattia <m.dantonio81@gmail.com>
2020-12-08 15:43:35 +0100

    # Added validation range to endpoint input model test

--- restapi/endpoints/test_inputs.py
+++ restapi/endpoints/test_inputs.py
@@ -1,13 +1,26 @@
+from datetime import datetime
+
+import pytz
+
 from restapi import decorators
 from restapi.config import TESTING
-from restapi.models import ISO8601UTC, Schema, fields
+from restapi.models import ISO8601UTC, Schema, fields, validate
 from restapi.rest.definition import EndpointResource
 
 if TESTING:
 
     class InputSchema(Schema):
-        mystr = fields.Str(required=True)
-        mydate = fields.DateTime(required=True, format=ISO8601UTC)
+        mystr = fields.Str(required=True, validate=validate.Length(min=1))
+        mydate = fields.DateTime(
+            required=True,
+            format=ISO8601UTC,
+            validate=validate.Range(
+                max=datetime.now(pytz.utc).replace(hour=23, minute=59, second=59),
+                min=datetime(1900, 1, 1, tzinfo=pytz.utc),
+                max_inclusive=True,
+                error="Invalid date",
+            ),
+        )
 
     class TestInputs(EndpointResource):
         @decorators.use_kwargs(InputSchema)
