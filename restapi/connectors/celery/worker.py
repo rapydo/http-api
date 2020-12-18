d8cbf4fbe5bda4f1db44c47dd166639387a83ff8
Mattia <m.dantonio81@gmail.com>
2020-12-18 07:05:01 +0100

    Moved flask app from CeleryExt.celery_app to CeleryExt

--- restapi/connectors/celery/worker.py
+++ restapi/connectors/celery/worker.py
@@ -2,11 +2,11 @@ from restapi.connectors import celery
 from restapi.server import ServerModes, create_app
 from restapi.utilities.logs import log
 
-# Used by Celery to run the instance (-A app)
-
-celery_app = celery.get_instance().celery_app
+instance = celery.get_instance()
+# Used by Celery to run the instance (-app app)
+celery_app = instance.celery_app
 
 # Reload Flask app code for the worker (needed to have the app context available)
-celery_app.app = create_app(mode=ServerModes.WORKER)
+instance.app = create_app(mode=ServerModes.WORKER)
 
 log.debug("Celery worker is ready {}", celery_app)
