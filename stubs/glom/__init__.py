5bfd4cf9755a2ae75a1e1e426887692ff91a58cd
Mattia <m.dantonio81@gmail.com>
2020-12-18 13:04:17 +0100

    # Simple test with mypy stubs /fix2

--- stubs/glom/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict
-
-
-class Spec:
-    # Glom is not really intended to return a str, but it is true in my case
-    def glom(self, target: Dict[str, Any], **kw: str) -> str:
-        ...
