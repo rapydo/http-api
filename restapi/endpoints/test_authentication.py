1923e7f25b709ef47a59b5a79ad7fad5a17e455a
Mattia <m.dantonio81@gmail.com>
2020-12-23 19:00:20 +0100

    # Debug code

--- restapi/endpoints/test_authentication.py
+++ restapi/endpoints/test_authentication.py
@@ -1,6 +1,7 @@
 from restapi import decorators
 from restapi.config import TESTING
 from restapi.rest.definition import EndpointResource
+from restapi.utilities.logs import log
 
 if TESTING:
 
@@ -32,6 +33,7 @@ if TESTING:
 
             resp = {}
             resp["token"] = self.get_token()
+            log.critical(resp["token"] is None)
             if user := self.get_user():
                 resp["user"] = user.email
             else:
