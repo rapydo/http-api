9c467c16a855bdfda94a240fb1b92158b4353fb3
Mattia <m.dantonio81@gmail.com>
2020-12-24 06:51:11 +0100

    # Tests fix

--- restapi/endpoints/test_cache.py
+++ restapi/endpoints/test_cache.py
@@ -23,8 +23,8 @@ if TESTING:
         @decorators.cache(timeout=1)
         def get(self):
 
-            TestSemiAuthCache.counter += 1
-            return self.response(TestSemiAuthCache.counter)
+            TestShortCache.counter += 1
+            return self.response(TestShortCache.counter)
 
     class TestLongCache(EndpointResource):
         """
@@ -45,8 +45,8 @@ if TESTING:
         @decorators.cache(timeout=200)
         def get(self):
 
-            TestSemiAuthCache.counter2 += 1
-            return self.response(TestSemiAuthCache.counter2)
+            TestLongCache.counter += 1
+            return self.response(TestLongCache.counter)
 
         @decorators.endpoint(
             path="/tests/cache/long",
@@ -86,7 +86,7 @@ if TESTING:
             TestAuthCache.counter += 1
             return self.response((self.get_user().uuid, TestAuthCache.counter))
 
-    class TestSemiAuthCache(EndpointResource):
+    class TestOptionalAuthCache(EndpointResource):
         """
         Used to test cache of optionally authenticated endpoints
         (cache keys are token dependent, if tokens are provided and valid)
@@ -115,5 +115,63 @@ if TESTING:
             else:
                 uuid = "N/A"
 
-            TestSemiAuthCache.counter += 1
-            return self.response((uuid, TestSemiAuthCache.counter))
+            TestOptionalAuthCache.counter += 1
+            return self.response((uuid, TestOptionalAuthCache.counter))
+
+    class TestParamAuthCache(EndpointResource):
+        """
+        Used to test cache of authenticated endpoints that accept access token param
+        (cache keys are token dependent)
+        """
+
+        labels = ["tests"]
+
+        counter = 0  # Increased at each request... except cached responses of course
+
+        @decorators.auth.require(allow_access_token_parameter=True)
+        @decorators.endpoint(
+            path="/tests/cache/paramauth",
+            summary="Execute tests of cached responses auth. endpoints with parameter",
+            description="Only enabled in testing mode",
+            responses={
+                200: "Content sent",
+            },
+        )
+        @decorators.cache(timeout=200)
+        def get(self):
+
+            TestParamAuthCache.counter += 1
+            return self.response((self.get_user().uuid, TestParamAuthCache.counter))
+
+    class TestOptionalParamAuthCache(EndpointResource):
+        """
+        Used to test cache of optionally authenticated endpoints
+        that accept access token param
+        (cache keys are token dependent, if tokens are provided and valid)
+        """
+
+        labels = ["tests"]
+
+        counter = 0  # Increased at each request... except cached responses of course
+
+        @decorators.auth.optional(allow_access_token_parameter=True)
+        @decorators.endpoint(
+            path="/tests/cache/optionalparamauth",
+            summary="Execute tests of caches of optionally auth. endpoints with param",
+            description="Only enabled in testing mode",
+            responses={
+                200: "Content sent",
+            },
+        )
+        @decorators.cache(timeout=200)
+        def get(self):
+
+            user = self.get_user()
+
+            if user:
+                uuid = user.uuid
+            else:
+                uuid = "N/A"
+
+            TestOptionalParamAuthCache.counter += 1
+            return self.response((uuid, TestOptionalParamAuthCache.counter))
