26ea3435076b81a8dc5801081a426857ba328991
Mattia <m.dantonio81@gmail.com>
2020-12-24 07:41:45 +0100

    # Test fix /2

--- tests/base/test_endpoints_caching.py
+++ tests/base/test_endpoints_caching.py
@@ -3,6 +3,9 @@ import time
 from restapi.services.cache import Cache
 from restapi.tests import API_URI, BaseTests
 
+UUID = 0
+COUNTER = 1
+
 
 class TestApp(BaseTests):
     def test_caching_autocleaning(self, client):
@@ -112,15 +115,15 @@ class TestApp(BaseTests):
         resp1 = self.get_content(r)
         assert isinstance(resp1, list)
         # counter is 1 because this is the first request to this endpoint
-        assert resp1[1] == 1
+        assert resp1[COUNTER] == 1
 
         r = client.get(f"{API_URI}/tests/cache/auth", headers=headers1)
         assert r.status_code == 200
         resp2 = self.get_content(r)
         assert isinstance(resp2, list)
-        assert resp2[0] == resp1[0]
+        assert resp2[UUID] == resp1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp2[1] == 1
+        assert resp2[COUNTER] == 1
 
         r = client.get(
             f"{API_URI}/tests/cache/auth", headers={"Authorization": "Bearer invalid"}
@@ -132,19 +135,18 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         resp2bis = self.get_content(r)
         assert isinstance(resp2bis, list)
-        assert resp2bis[0] == resp1[0]
+        assert resp2bis[UUID] == resp1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp2bis[1] == 1
+        assert resp2bis[COUNTER] == 1
 
         # Same user but different token, the cache should not be used
         r = client.get(f"{API_URI}/tests/cache/auth", headers=headers2)
         assert r.status_code == 200
         resp3 = self.get_content(r)
         assert isinstance(resp3, list)
-        assert resp3[0] != resp1[0]
-        assert resp3[0] != resp2[0]
-        # Same counter as above, because the response is replied from the cache
-        assert resp3[1] == 2
+        assert resp3[UUID] == resp1[UUID]
+        assert resp3[UUID] == resp2[UUID]
+        assert resp3[COUNTER] == 2
 
         # Create a new user on the fly to test the cached endpoint
         schema = self.getDynamicInputSchema(client, "admin/users", headers1)
@@ -161,22 +163,22 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         resp4 = self.get_content(r)
         assert isinstance(resp4, list)
-        assert resp4[0] == uuid
-        assert resp4[0] != resp1[0]
-        assert resp4[0] != resp2[0]
-        assert resp4[0] != resp3[0]
+        assert resp4[UUID] == uuid
+        assert resp4[UUID] != resp1[UUID]
+        assert resp4[UUID] != resp2[UUID]
+        assert resp4[UUID] != resp3[UUID]
         # The counter changed, because the response is not replied from the cache
-        assert resp4[1] == 3
+        assert resp4[COUNTER] == 3
 
         # Same token, response must be cached
         r = client.get(f"{API_URI}/tests/cache/auth", headers=headers3)
         assert r.status_code == 200
         resp5 = self.get_content(r)
         assert isinstance(resp5, list)
-        assert resp5[0] == uuid
-        assert resp5[0] == resp4[0]
+        assert resp5[UUID] == uuid
+        assert resp5[UUID] == resp4[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp5[1] == 3
+        assert resp5[COUNTER] == 3
 
         # Delete the user created on the fly to test the cache
         r = client.delete(f"{API_URI}/admin/users/{uuid}", headers=headers1)
@@ -187,26 +189,26 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         nonauthenticated1 = self.get_content(r)
         assert isinstance(nonauthenticated1, list)
-        assert nonauthenticated1[0] == "N/A"
+        assert nonauthenticated1[UUID] == "N/A"
         # counter is 1 because this is the first request to this endpoint
-        assert nonauthenticated1[1] == 1
+        assert nonauthenticated1[COUNTER] == 1
 
         r = client.get(f"{API_URI}/tests/cache/optionalauth")
         assert r.status_code == 200
         nonauthenticated2 = self.get_content(r)
         assert isinstance(nonauthenticated2, list)
-        assert nonauthenticated2[0] == "N/A"
+        assert nonauthenticated2[UUID] == "N/A"
         # Same counter as above, because the response is replied from the cache
-        assert nonauthenticated2[1] == 1
+        assert nonauthenticated2[COUNTER] == 1
 
         headers, _ = self.do_login(client, None, None)
         r = client.get(f"{API_URI}/tests/cache/optionalauth", headers=headers)
         assert r.status_code == 200
         authenticated1 = self.get_content(r)
         assert isinstance(authenticated1, list)
-        assert authenticated1[0] != "N/A"
+        assert authenticated1[UUID] != "N/A"
         # The counter changed, because the response is not replied from the cache
-        assert authenticated1[1] == 2
+        assert authenticated1[COUNTER] == 2
 
         # Token cached => cache should be used
         headers, _ = self.do_login(client, None, None)
@@ -214,9 +216,9 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         authenticated2 = self.get_content(r)
         assert isinstance(authenticated2, list)
-        assert authenticated2[0] != authenticated1[0]
+        assert authenticated2[UUID] != authenticated1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert authenticated2[1] == 3
+        assert authenticated2[COUNTER] == 3
 
         r = client.get(
             f"{API_URI}/tests/cache/optionalauth",
@@ -234,16 +236,16 @@ class TestApp(BaseTests):
         resp1 = self.get_content(r)
         assert isinstance(resp1, list)
         # counter is 1 because this is the first request to this endpoint
-        assert resp1[1] == 1
+        assert resp1[COUNTER] == 1
 
         r = client.get(f"{API_URI}/tests/cache/auth", headers=headers1)
         assert r.status_code == 200
         resp2 = self.get_content(r)
         assert isinstance(resp2, list)
-        assert resp2[0] == resp1[0]
+        assert resp2[UUID] == resp1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp2[1] == resp1[1]
-        assert resp2[1] == 1
+        assert resp2[COUNTER] == resp1[COUNTER]
+        assert resp2[COUNTER] == 1
 
         # Test by using access_token parameter instead of Headers
         r = client.get(
@@ -253,10 +255,10 @@ class TestApp(BaseTests):
         resp3 = self.get_content(r)
         assert isinstance(resp3, list)
         # This is the same user, uuid is unchanged
-        assert resp3[0] == resp1[0]
+        assert resp3[UUID] == resp1[UUID]
         # but counter changed, because the response is not replied from the cache
-        assert resp3[1] != resp1[1]
-        assert resp3[1] == 2
+        assert resp3[COUNTER] != resp1[COUNTER]
+        assert resp3[COUNTER] == 2
 
         r = client.get(
             f"{API_URI}/tests/cache/auth", query_string={"access_token": token2}
@@ -264,10 +266,10 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         resp4 = self.get_content(r)
         assert isinstance(resp4, list)
-        assert resp4[0] == resp1[0]
+        assert resp4[UUID] == resp1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp4[1] == resp3[1]
-        assert resp4[1] == 2
+        assert resp4[COUNTER] == resp3[COUNTER]
+        assert resp4[COUNTER] == 2
 
         # Cache is stored from access_token parameter,
         # but the token is the same if provided in the headers
@@ -275,27 +277,27 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         resp5 = self.get_content(r)
         assert isinstance(resp5, list)
-        assert resp5[0] == resp1[0]
+        assert resp5[UUID] == resp1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert resp5[1] == resp3[1]
-        assert resp5[1] == 2
+        assert resp5[COUNTER] == resp3[COUNTER]
+        assert resp5[COUNTER] == 2
 
     def test_cached_semiauthenticated_param_endpoint(self, client):
         r = client.get(f"{API_URI}/tests/cache/optionalauth")
         assert r.status_code == 200
         nonauthenticated1 = self.get_content(r)
         assert isinstance(nonauthenticated1, list)
-        assert nonauthenticated1[0] == "N/A"
+        assert nonauthenticated1[UUID] == "N/A"
         # counter is 1 because this is the first request to this endpoint
-        assert nonauthenticated1[1] == 1
+        assert nonauthenticated1[COUNTER] == 1
 
         r = client.get(f"{API_URI}/tests/cache/optionalauth")
         assert r.status_code == 200
         nonauthenticated2 = self.get_content(r)
         assert isinstance(nonauthenticated2, list)
-        assert nonauthenticated2[0] == "N/A"
+        assert nonauthenticated2[UUID] == "N/A"
         # Same counter as above, because the response is replied from the cache
-        assert nonauthenticated2[1] == 1
+        assert nonauthenticated2[COUNTER] == 1
 
         headers1, token1 = self.do_login(client, None, None)
         headers2, token2 = self.do_login(client, None, None)
@@ -304,18 +306,18 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         authenticated1 = self.get_content(r)
         assert isinstance(authenticated1, list)
-        assert authenticated1[0] != "N/A"
+        assert authenticated1[UUID] != "N/A"
         # The counter changed, because the response is not replied from the cache
-        assert authenticated1[1] == 2
+        assert authenticated1[COUNTER] == 2
 
         r = client.get(f"{API_URI}/tests/cache/optionalauth", headers=headers1)
         assert r.status_code == 200
         authenticated2 = self.get_content(r)
         assert isinstance(authenticated2, list)
-        assert authenticated2[0] != "N/A"
-        assert authenticated2[0] == authenticated1[0]
+        assert authenticated2[UUID] != "N/A"
+        assert authenticated2[UUID] == authenticated1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert authenticated2[1] == 2
+        assert authenticated2[COUNTER] == 2
 
         # Response is reply from the cache even if the token is provided as query param
         r = client.get(
@@ -324,10 +326,10 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         authenticated3 = self.get_content(r)
         assert isinstance(authenticated3, list)
-        assert authenticated3[0] != "N/A"
-        assert authenticated3[0] == authenticated1[0]
+        assert authenticated3[UUID] != "N/A"
+        assert authenticated3[UUID] == authenticated1[UUID]
         # Same counter as above, because the response is replied from the cache
-        assert authenticated3[1] == 2
+        assert authenticated3[COUNTER] == 2
 
         # Let's test another token => new cache key.
         # This time access_token and then headers
@@ -338,11 +340,11 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         authenticated4 = self.get_content(r)
         assert isinstance(authenticated4, list)
-        assert authenticated4[0] != "N/A"
+        assert authenticated4[UUID] != "N/A"
         # Different token, but the user is the same
-        assert authenticated4[0] == authenticated1[0]
+        assert authenticated4[UUID] == authenticated1[UUID]
         # Counter increased
-        assert authenticated4[1] == 3
+        assert authenticated4[COUNTER] == 3
 
         r = client.get(
             f"{API_URI}/tests/cache/optionalauth", query_string={"access_token": token2}
@@ -350,20 +352,20 @@ class TestApp(BaseTests):
         assert r.status_code == 200
         authenticated5 = self.get_content(r)
         assert isinstance(authenticated5, list)
-        assert authenticated5[0] != "N/A"
+        assert authenticated5[UUID] != "N/A"
         # Different token, but the user is the same
-        assert authenticated5[0] == authenticated1[0]
+        assert authenticated5[UUID] == authenticated1[UUID]
         # Counter increased
-        assert authenticated5[1] == authenticated4[1]
-        assert authenticated5[1] == 3
+        assert authenticated5[COUNTER] == authenticated4[COUNTER]
+        assert authenticated5[COUNTER] == 3
 
         r = client.get(f"{API_URI}/tests/cache/optionalauth", headers=headers2)
         assert r.status_code == 200
         authenticated6 = self.get_content(r)
         assert isinstance(authenticated6, list)
-        assert authenticated6[0] != "N/A"
+        assert authenticated6[UUID] != "N/A"
         # Different token, but the user is the same
-        assert authenticated6[0] == authenticated1[0]
+        assert authenticated6[UUID] == authenticated1[UUID]
         # Counter increased
-        assert authenticated6[1] == authenticated4[1]
-        assert authenticated6[1] == 3
+        assert authenticated6[COUNTER] == authenticated4[COUNTER]
+        assert authenticated6[COUNTER] == 3
