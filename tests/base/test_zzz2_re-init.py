07a7f19cf2b5d367bc18305891d84152bc8fb695
Mattia <m.dantonio81@gmail.com>
2021-01-22 06:27:33 +0100

    # Tests debugging

--- tests/base/test_zzz2_re-init.py
+++ tests/base/test_zzz2_re-init.py
@@ -1,47 +1,14 @@
+"""
+This test is intended to be executed as last, just after the destroy test
+(this is why it is prefixed as zzz2)
+Beware: if env TEST_DESTROY_MODE == 1 this test will destroy your database, be careful
+"""
 import os
-import time
 
-from restapi.connectors import Connector, sqlalchemy
-from restapi.exceptions import ServiceUnavailable
+from restapi.connectors import Connector
 from restapi.server import ServerModes, create_app
 from restapi.services.authentication import BaseAuthentication
 from restapi.utilities.logs import log
-
-
-def test_destroy() -> None:
-
-    # Only executed if tests are run with --destroy flag
-    if os.getenv("TEST_DESTROY_MODE", "0") != "1":
-        log.info("Skipping destroy test, TEST_DESTROY_MODE not enabled")
-        return
-
-    # Always enable during core tests
-    if not Connector.check_availability("authentication"):  # pragma: no cover
-        log.warning("Skipping authentication test: service not available")
-        return
-
-    # if Connector.check_availability("sqlalchemy"):
-    #     sql = sqlalchemy.get_instance()
-    #     # Close previous connections, otherwise the new create_app will hang
-    #     sql.session.remove()
-    #     sql.session.close_all()
-
-    auth = Connector.get_authentication_instance()
-
-    user = auth.get_user(username=BaseAuthentication.default_user)
-    assert user is not None
-
-    create_app(mode=ServerModes.DESTROY)
-
-    try:
-        auth = Connector.get_authentication_instance()
-        user = auth.get_user(username=BaseAuthentication.default_user)
-        assert user is None
-    except ServiceUnavailable:
-        pass
-
-    Connector.disconnect_all()
-    time.sleep(2)
 
 
 def test_init() -> None:
