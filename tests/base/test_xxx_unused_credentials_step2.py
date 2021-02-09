915b8247ca0933ebc6789e829f765f8ed28b3f77
Mattia <m.dantonio81@gmail.com>
2021-02-09 12:05:22 +0100

    Added tests on unused credentials ban

--- tests/base/test_xxx_unused_credentials_step2.py
+++ tests/base/test_xxx_unused_credentials_step2.py
@@ -3,7 +3,7 @@ This will used credentials created at the beginning of the suite
 to verify that unused credentialas are banned
 """
 from restapi.env import Env
-from restapi.tests import BaseTests, FlaskClient
+from restapi.tests import AUTH_URI, BaseTests, FlaskClient
 
 if Env.get_int("AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER") > 0:
 
@@ -13,11 +13,23 @@ if Env.get_int("AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER") > 0:
             assert BaseTests.unused_credentials is not None
             assert len(BaseTests.unused_credentials) == 3
 
-            # This test will fail because the credentials should be banned!!
-            headers, _ = self.do_login(
-                client, BaseTests.unused_credentials[0], BaseTests.unused_credentials[1]
-            )
-            assert headers is not None
+            data = {
+                "username": BaseTests.unused_credentials[0],
+                "password": BaseTests.unused_credentials[1],
+            }
+
+            # Login is blocked due to inactivity
+            r = client.post(f"{AUTH_URI}/login", data=data)
+            assert r.status_code == 401
+            resp = self.get_content(r)
+            assert resp == "Sorry, this account is blocked for inactivity"
+
+            # Also password reset and blocked... how to recover the account !?
+
+            reset_data = {"reset_email": BaseTests.unused_credentials[0]}
+            r = client.post(f"{AUTH_URI}/reset", data=reset_data)
+            resp = self.get_content(r)
+            assert resp == "Sorry, this account is blocked for inactivity"
 
             # Goodbye temporary user
             self.delete_user(client, BaseTests.unused_credentials[2])
