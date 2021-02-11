3b69d1f64978a111fba71421f60ef310205b7316
Mattia <m.dantonio81@gmail.com>
2021-02-11 15:55:09 +0100

    # Improved inactivity test

--- tests/base/test_xxx_unused_credentials_step2.py
+++ tests/base/test_xxx_unused_credentials_step2.py
@@ -2,16 +2,31 @@
 This will used credentials created at the beginning of the suite
 to verify that unused credentialas are banned
 """
+from faker import Faker
+
 from restapi.env import Env
 from restapi.tests import AUTH_URI, BaseTests, FlaskClient
 
 if Env.get_int("AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER") > 0:
 
     class TestApp1(BaseTests):
-        def test_test_unused_credentials(self, client: FlaskClient) -> None:
+        def test_test_unused_credentials(
+            self, client: FlaskClient, fake: Faker
+        ) -> None:
 
             assert BaseTests.unused_credentials is not None
             assert len(BaseTests.unused_credentials) == 3
+
+            data = {
+                "username": BaseTests.unused_credentials[0],
+                "password": fake.password(strong=True),
+            }
+
+            # Credentials are verified before the inactivity check
+            r = client.post(f"{AUTH_URI}/login", data=data)
+            assert r.status_code == 401
+            resp = self.get_content(r)
+            assert resp == "Invalid access credentials"
 
             data = {
                 "username": BaseTests.unused_credentials[0],
