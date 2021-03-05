0d5471a59516b4429aadaec92e218e880b62b061
Mattia <m.dantonio81@gmail.com>
2021-03-05 06:41:16 +0100

    # Bug fix: the new models module was missing in the previous commit

---
+++ restapi/models/fields.py
@@ -0,0 +1,259 @@
+import json
+import re
+
+from marshmallow import ValidationError
+from webargs import fields as webargs_fields
+
+from restapi.config import TESTING
+from restapi.utilities.logs import log
+
+
+class Field(webargs_fields.Field):
+    def __init__(
+        self,
+        *args,
+        label=None,
+        description=None,
+        password=False,
+        autocomplete=None,
+        **kwargs,
+    ):
+
+        kwargs.setdefault("metadata", {})
+        kwargs["metadata"].setdefault("label", label)
+        kwargs["metadata"].setdefault("description", description)
+        kwargs["metadata"].setdefault("password", password)
+        kwargs["metadata"].setdefault("autocomplete", autocomplete)
+
+        super().__init__(*args, **kwargs)
+
+    pass
+
+
+####################################################################################
+#               1) Include all types from webargs to this custom module
+####################################################################################
+
+
+class AwareDateTime(Field, webargs_fields.AwareDateTime):
+    pass
+
+
+class Bool(Field, webargs_fields.Bool):
+    pass
+
+
+class Boolean(Field, webargs_fields.Boolean):
+    pass
+
+
+class Constant(Field, webargs_fields.Constant):
+    pass
+
+
+class Date(Field, webargs_fields.Date):
+    pass
+
+
+class DateTime(Field, webargs_fields.DateTime):
+    pass
+
+
+class Decimal(Field, webargs_fields.Decimal):
+    pass
+
+
+class Dict(Field, webargs_fields.Dict):
+    pass
+
+
+class Email(Field, webargs_fields.Email):
+    pass
+
+
+class Float(Field, webargs_fields.Float):
+    pass
+
+
+class Function(Field, webargs_fields.Function):
+    pass
+
+
+class Int(Field, webargs_fields.Int):
+    pass
+
+
+class Integer(Field, webargs_fields.Integer):
+    pass
+
+
+class Mapping(Field, webargs_fields.Mapping):
+    pass
+
+
+class Method(Field, webargs_fields.Method):
+    pass
+
+
+class NaiveDateTime(Field, webargs_fields.NaiveDateTime):
+    pass
+
+
+class Number(Field, webargs_fields.Number):
+    pass
+
+
+class Raw(Field, webargs_fields.Raw):
+    pass
+
+
+class Str(Field, webargs_fields.Str):
+    pass
+
+
+class String(Field, webargs_fields.String):
+    pass
+
+
+class TimeDelta(Field, webargs_fields.TimeDelta):
+    pass
+
+
+class URL(Field, webargs_fields.URL):
+    pass
+
+
+class Url(Field, webargs_fields.Url):
+    pass
+
+
+class UUID(Field, webargs_fields.UUID):
+    pass
+
+
+####################################################################################
+#   2) Override some types with custom implementation to extend functionalities
+####################################################################################
+class List(Field, webargs_fields.List):
+    def __init__(self, *args, unique=False, min_items=0, **kwargs):
+        self.unique = unique
+        self.min_items = min_items
+
+        super().__init__(*args, **kwargs)
+
+    def _deserialize(self, value, attr, data, **kwargs):
+
+        # this is the case when requests (or pytest) send some json-dumped lists
+        # for example for a multi-value select
+        if isinstance(value, str):
+            try:
+                value = json.loads(value)
+            except BaseException as e:
+                log.warning(e)
+
+        value = super()._deserialize(value, attr, data, **kwargs)
+
+        if not isinstance(value, list):  # pragma: no cover
+            raise ValidationError("Invalid type")
+
+        if self.unique:
+            value = list(set(value))
+
+        if len(value) < self.min_items:
+            raise ValidationError(
+                f"Expected at least {self.min_items} items, received {len(value)}"
+            )
+
+        return value
+
+
+class Nested(Field, webargs_fields.Nested):
+    def _deserialize(self, value, attr, data, **kwargs):
+
+        # this is the case when requests (or pytest) send some json-dumped object
+        if isinstance(value, str):
+            try:
+                value = json.loads(value)
+            except BaseException as e:  # pragma: no cover
+                log.warning(e)
+        super()._deserialize(value, attr, data, **kwargs)
+
+
+class DelimitedList(Field, webargs_fields.DelimitedList):
+    def __init__(self, *args, unique=False, **kwargs):
+        self.unique = unique
+
+        super().__init__(*args, **kwargs)
+
+    def _deserialize(self, value, attr, data, **kwargs):
+
+        values = super()._deserialize(value, attr, data, **kwargs)
+
+        if self.unique and len(values) != len(set(values)):
+            raise ValidationError("Provided list contains duplicates")
+
+        return values
+
+
+####################################################################################
+#                   3) Add some additional custom fields
+####################################################################################
+
+
+class Neo4jChoice(Field):
+    """Field that serializes from a neo4j choice"""
+
+    # choice_model is the same used in neo4j model as choices=
+    def __init__(self, choices_model, **kwargs):
+        super().__init__(**kwargs)
+        if isinstance(choices_model, dict):
+            self.choices_dict = choices_model
+        else:
+            # convert the tuple of tuple into as a dictionary for convenience
+            self.choices_dict = {}
+            for k, v in choices_model:
+                self.choices_dict.setdefault(k, v)
+
+    def _serialize(self, value, attr, obj, **kwargs):
+        return {
+            "key": value,
+            # the value correspondance from choices_dict or value as default
+            "description": self.choices_dict.get(value, value),
+        }
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        return value
+
+
+class Neo4jRelationshipToMany(Nested):
+    # nested_obj: StructuredRel
+    def _serialize(self, nested_obj, attr, obj, **kwargs):
+        self.many = True
+        return super()._serialize(nested_obj.all(), attr, obj, **kwargs)
+
+
+class Neo4jRelationshipToSingle(Nested):
+    # nested_obj: StructuredRel
+    def _serialize(self, nested_obj, attr, obj, **kwargs):
+        self.many = False
+        self.schema.many = False
+        return super()._serialize(nested_obj.single(), attr, obj, **kwargs)
+
+
+class Neo4jRelationshipToCount(Int):
+    # value: StructuredRel
+    def _serialize(self, value, attr, obj, **kwargs):
+        return self._format_num(len(value))
+
+
+class TOTP(String):
+    def _deserialize(self, value, attr, data, **kwargs):
+
+        value = super()._deserialize(value, attr, data, **kwargs)
+
+        if not re.match(r"^[0-9]{6}$", value):
+            if TESTING:
+                log.error("Invalid TOTP format: {}", value)
+            raise ValidationError("Invalid TOTP format")
+
+        return value
---
+++ restapi/models/schema.py
@@ -0,0 +1,132 @@
+import inspect
+from typing import Dict, Union
+
+import simplejson
+from marshmallow import EXCLUDE
+from marshmallow import Schema as MarshmallowSchema
+from marshmallow import ValidationError, pre_load
+from neomodel import StructuredNode, StructuredRel, properties
+
+from restapi.models import fields
+from restapi.utilities.logs import log
+
+GET_SCHEMA_KEY = "get_schema"
+
+
+class Schema(MarshmallowSchema):
+    def __init__(self, strip_required=False, *args, **kwargs):
+
+        super().__init__(**kwargs)
+        if strip_required:
+            for k in self.declared_fields:
+                self.declared_fields[k].required = False
+
+    # Mypy does not accept the equivalence beetween
+    # marshmallow.fields and restapi.models.fields
+    # And cannot be blamed for that... it's a dirty implementation :-)
+    @classmethod
+    def from_dict(  # type: ignore
+        cls, attributes: Dict[str, Union[fields.Field, type]], name: str
+    ) -> type:
+        return super().from_dict(attributes, name=name)  # type: ignore
+
+    # instruct marshmallow to serialize data to a collections.OrderedDict
+    class Meta:
+        ordered = True
+        render_module = simplejson
+
+    # NOTE: self is not used, but @pre_load cannot be static
+    @pre_load
+    def raise_get_schema(self, data, **kwargs):
+
+        if "access_token" in data:
+            # valid for ImmutableMultiDict:
+            data = data.to_dict()
+            data.pop("access_token")
+
+        if GET_SCHEMA_KEY in data:
+            raise ValidationError("Schema requested")
+        return data
+
+
+class PartialSchema(Schema):
+    class Meta:
+        ordered = True
+        unknown = EXCLUDE
+
+
+class Neo4jSchema(Schema):
+    def __init__(self, model, fields, *args, **kwargs):
+        super().__init__(**kwargs)
+
+        if not fields:
+            fields = ()
+        elif fields == "*":
+            fields = None
+        elif fields[0] == "*":
+            fields = None
+        elif isinstance(fields, tuple):
+            pass
+        elif isinstance(fields, list):
+            pass
+        else:  # pragma: no cover
+            log.error("Invalid fields: {}", fields)
+            fields = ()
+
+        self.fields = fields
+        # Leave the constructor to avoid variable shadowing between
+        # this fields and the from marshmallow import fields above
+        self.build_schema(model)
+
+    def build_schema(self, model):
+
+        # Get the full list of parent classes from model to object
+        classes = inspect.getmro(model)
+
+        starting_point = False
+        # Iterate in reversed order to start from object
+        for c in reversed(classes):
+            # Skip all parentes up to StructuredNode and StructuredRel (included)
+            if not starting_point:
+                # Found the starting point, next class will be descended up to model
+                if c == StructuredNode or c == StructuredRel:
+                    starting_point = True
+                # skip all parent up to StructuredNode and StructuredRel INCLUDED
+                continue
+
+            # Iterate all class attributes to find neomodel properties
+            for attribute in c.__dict__:
+                prop = getattr(c, attribute)
+
+                if not isinstance(prop, properties.Property):
+                    continue
+
+                # self.fields can be None when the special value * is given in input
+                if self.fields and attribute not in self.fields:
+                    continue
+
+                # log.info("Including property {}.{}", model.__name__, attribute)
+                if isinstance(prop, properties.StringProperty):
+                    if prop.choices is None:
+                        self.declared_fields[attribute] = fields.Str()
+                    else:
+                        self.declared_fields[attribute] = fields.Neo4jChoice(
+                            prop.choices
+                        )
+
+                elif isinstance(prop, properties.BooleanProperty):
+                    self.declared_fields[attribute] = fields.Boolean()
+                elif isinstance(prop, properties.IntegerProperty):
+                    self.declared_fields[attribute] = fields.Integer()
+                elif isinstance(prop, properties.EmailProperty):
+                    self.declared_fields[attribute] = fields.Email()
+                elif isinstance(prop, properties.DateTimeProperty):
+                    self.declared_fields[attribute] = fields.AwareDateTime()
+                elif isinstance(prop, properties.UniqueIdProperty):
+                    self.declared_fields[attribute] = fields.Str()
+                else:  # pragma: no cover
+                    log.error(
+                        "Unsupport neomodel property: {}, fallback to StringProperty",
+                        prop.__class__.__name__,
+                    )
+                    self.declared_fields[attribute] = fields.Str()
