270a8b66604919f6d727e46c3602adb7cf782509
Mattia <m.dantonio81@gmail.com>
2021-03-06 22:07:59 +0100

    Added tests on autocomplete endpoints

---
+++ tests/base/test_endpoints_autocomplete.py
@@ -0,0 +1,61 @@
+import json
+import random
+
+from restapi.tests import API_URI, BaseTests, FlaskClient
+
+
+class TestApp(BaseTests):
+    def test_autocomplete(self, client: FlaskClient) -> None:
+
+        # This test verifies that buildData is always able to randomly create
+        # valid inputs for endpoints with inputs defined by marshamallow schemas
+        schema = self.getDynamicInputSchema(client, "tests/autocomplete", {})
+
+        assert schema[0]["key"] == "elements"
+        assert schema[0]["type"] == "string[]"
+        assert "autocomplete_endpoint" in schema[0]
+        assert "autocomplete_id_bind" in schema[0]
+        assert "autocomplete_label_bind" in schema[0]
+        assert "autocomplete_show_id" in schema[0]
+        assert schema[0]["autocomplete_endpoint"] == "tests/autocomplete"
+        assert schema[0]["autocomplete_id_bind"] == "my_id"
+        assert schema[0]["autocomplete_label_bind"] == "my_label"
+        assert schema[0]["autocomplete_show_id"] is True
+
+        autocomplete_endpoint = f"{API_URI}/{schema[0]['autocomplete_endpoint']}"
+
+        r = client.get(f"{autocomplete_endpoint}/nobody")
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) == 0
+
+        r = client.get(f"{autocomplete_endpoint}/oliver")
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) > 0
+        assert schema[0]["autocomplete_id_bind"] in content[0]
+        assert schema[0]["autocomplete_label_bind"] in content[0]
+
+        r = client.get(f"{autocomplete_endpoint}/s the")
+        assert r.status_code == 200
+        content = self.get_content(r)
+        assert len(content) > 0
+        assert schema[0]["autocomplete_id_bind"] in content[0]
+        assert schema[0]["autocomplete_label_bind"] in content[0]
+
+        rand = random.SystemRandom()
+
+        data = []
+        for _ in range(0, 3):
+            element = rand.choice(content)
+            data.append(element[schema[0]["autocomplete_id_bind"]])
+
+        # put accepts a single id provided by the autocomplete endpoint
+        r = client.put(f"{API_URI}/tests/autocomplete", data={"element": data[0]})
+        assert r.status_code == 204
+
+        # post accepts a list of ids provided by the autocomplete endpoint
+        r = client.post(
+            f"{API_URI}/tests/autocomplete", data={"elements": json.dumps(data)}
+        )
+        assert r.status_code == 204
