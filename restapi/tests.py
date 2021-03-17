26cce8fe5cedc66384912cdf83998780b008801f
Mattia <m.dantonio81@gmail.com>
2021-03-17 13:24:56 +0100

    # Type fix

--- restapi/tests.py
+++ restapi/tests.py
@@ -96,6 +96,7 @@ class BaseTests:
     @staticmethod
     def get_content(http_out: Response) -> Any:
 
+        http_out.status_code == 200
         try:
             response = json.loads(http_out.get_data().decode())
         except Exception as e:  # pragma: no cover
