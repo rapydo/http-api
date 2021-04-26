9a59fda0465abe8b395316f98772c3083a9e3c22
Mattia <m.dantonio81@gmail.com>
2021-04-26 11:47:56 +0200

    # No-Auth Tests fix: schemathesis configuration

--- tests/base/test_yyy_schemathesis.py
+++ tests/base/test_yyy_schemathesis.py
@@ -51,17 +51,22 @@ else:
     set_logger("WARNING")
     app = create_app()
     client = werkzeug.Client(app, werkzeug.wrappers.Response)
-    BaseAuthentication.load_default_user()
-    BaseAuthentication.load_roles()
-    USER = BaseAuthentication.default_user
-    PWD = BaseAuthentication.default_password
-    data = {"username": USER, "password": PWD}
-    token, auth_header = get_auth_token(client, data)
 
-    # it does not handle custom headers => the endpoint will provide partial schema
-    # due to missing authentication => skipping all private endpoints and schemas
-    # schema = schemathesis.from_wsgi('/api/specs', app)
-    r = client.get(f"/api/specs?access_token={token}")
+    if Env.get_bool("AUTH_ENABLED"):
+        BaseAuthentication.load_default_user()
+        BaseAuthentication.load_roles()
+        USER = BaseAuthentication.default_user
+        PWD = BaseAuthentication.default_password
+        data = {"username": USER, "password": PWD}
+        token, auth_header = get_auth_token(client, data)
+
+        # it does not handle custom headers => the endpoint will provide partial schema
+        # due to missing authentication => skipping all private endpoints and schemas
+        # schema = schemathesis.from_wsgi('/api/specs', app)
+        r = client.get(f"/api/specs?access_token={token}")
+    else:
+        r = client.get("/api/specs")
+
     assert r.status_code == 200
     schema = json.loads(r.get_data().decode())
     schema = schemathesis.from_dict(schema, app=app)
@@ -87,49 +92,47 @@ else:
         # https://github.com/kiwicom/schemathesis/blob/master/src/schemathesis/checks.py#L99
         case.validate_response(response)
 
-    @schema.parametrize()
-    @settings(
-        deadline=None,
-        suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much],
-        max_examples=50,
-    )
-    def test_with_admin_auth(case):
+    if Env.get_bool("AUTH_ENABLED"):
 
-        if case.path == "/auth/logout":
-            # log.warning("Skipping logout")
-            return None
+        @schema.parametrize()
+        @settings(
+            deadline=None,
+            suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much],
+            max_examples=50,
+        )
+        def test_with_auth(case):
 
-        if case.headers is None:
-            case.headers = auth_header
+            if case.path == "/auth/logout":
+                # log.warning("Skipping logout")
+                return None
 
-        response = case.call_wsgi()
+            if case.headers is None:
+                case.headers = auth_header
 
-        # I want to allow 503 errors, raised in case of mail sending not enabled
-        # Let's convert to 404 errors
-        if response.status_code == 503:  # pragma: no cover
-            response.status_code = 404
+            response = case.call_wsgi()
 
-        # validation checks are defined here:
-        # https://github.com/kiwicom/schemathesis/blob/master/src/schemathesis/checks.py#L99
-        case.validate_response(response)
+            # I want to allow 503 errors, raised in case of mail sending not enabled
+            # Let's convert to 404 errors
+            if response.status_code == 503:  # pragma: no cover
+                response.status_code = 404
 
-    # FIXME: TO BE IMPLEMENTED
-    # def test_with_user_auth(case):
-    #     pass
+            # validation checks are defined here:
+            # https://github.com/kiwicom/schemathesis/blob/master/src/schemathesis/checks.py#L99
+            case.validate_response(response)
 
-    @schema.parametrize(endpoint="/auth/logout")
-    @settings(
-        deadline=None,
-        suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much],
-        max_examples=50,
-    )
-    def test_logout(case):
+        @schema.parametrize(endpoint="/auth/logout")
+        @settings(
+            deadline=None,
+            suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much],
+            max_examples=50,
+        )
+        def test_logout(case):
 
-        if case.headers is None:  # pragma: no cover
-            case.headers = auth_header
+            if case.headers is None:  # pragma: no cover
+                case.headers = auth_header
 
-        response = case.call_wsgi()
+            response = case.call_wsgi()
 
-        # validation checks are defined here:
-        # https://github.com/kiwicom/schemathesis/blob/master/src/schemathesis/checks.py#L99
-        case.validate_response(response)
+            # validation checks are defined here:
+            # https://github.com/kiwicom/schemathesis/blob/master/src/schemathesis/checks.py#L99
+            case.validate_response(response)
