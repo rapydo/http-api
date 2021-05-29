0f1673815d1a40aa4a1d1ffa8aa7db6f743c9d0c
Mattia <m.dantonio81@gmail.com>
2021-05-29 19:35:59 +0200

    Upgraded schemathesis from 3.7.2 to 3.7.4

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         "pytest-flask==1.2.0",
         "pytest-cov==2.12.0",
         "pytest-timeout==1.4.2",
-        "schemathesis==3.7.2",
+        "schemathesis==3.7.4",
         "Faker==8.4.0",
         "Telethon==1.21.1",
     ],
