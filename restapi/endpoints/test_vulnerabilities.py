fb01fb655b500773db9314ce5b821adf159506f9
Mattia <m.dantonio81@gmail.com>
2021-05-31 22:22:43 +0200

    # Stub of vulnerabilities test

--- restapi/endpoints/test_vulnerabilities.py
+++ restapi/endpoints/test_vulnerabilities.py
@@ -1,6 +1,7 @@
 from restapi import decorators
 from restapi.config import TESTING
 from restapi.connectors import Connector, neo4j, sqlalchemy
+from restapi.models import fields
 from restapi.rest.definition import EndpointResource, Response
 
 # from restapi.utilities.logs import log
@@ -11,28 +12,53 @@ if TESTING:
 
         labels = ["tests"]
 
-        @decorators.endpoint(
-            path="/tests/vulnerabilities/<test>",
-            summary="Execute tests against known vulnerabilities",
-            description="Only enabled in testing mode",
-            responses={200: "Tests executed"},
-        )
-        def get(self, test: str) -> Response:
+        def do_queries(self, value: str) -> None:
+
+            # Temporary disabled
+            value = "value"
 
             # This is just a stub... to be completed
             if Connector.check_availability("neo4j"):
                 graph = neo4j.get_instance()
-                graph.cypher(f"MATCH (u: User) WHERE u.name = '{test}' return u.name")
-                graph.cypher(f'MATCH (u: User) WHERE u.name = "{test}" return u.name')
-                graph.cypher(f"MATCH (u: User) return u.name as {test}")
-                graph.cypher(f"MATCH (u: User) return u.name as {test}")
-                graph.User.nodes.get_or_none(name=test)
+
+                graph.cypher(f"MATCH (u: User) WHERE u.name = '{value}' return u.name")
+                graph.cypher(f'MATCH (u: User) WHERE u.name = "{value}" return u.name')
+                graph.cypher(f"MATCH (u: User) return u.name as {value}")
+                graph.cypher(f"MATCH (u: User) return u.name as {value}")
+                graph.User.nodes.get_or_none(name=value)
+
             elif Connector.check_availability("sqlalchemy"):
                 sql = sqlalchemy.get_instance()
-                sql.execute(f"SELECT name FROM User WHERE u.name = '{test}'")
-                sql.execute(f'SELECT name FROM User WHERE u.name = "{test}"')
-                sql.execute(f"SELECT name as {test} FROM User")
-                sql.execute(f"SELECT name as {test} FROM User")
-                sql.User.query.filter_by(name=test).first()
 
+                sql.db.execute(f"SELECT name FROM User WHERE u.name = '{value}'")
+                sql.db.execute(f'SELECT name FROM User WHERE u.name = "{value}"')
+                sql.db.execute(f"SELECT name as {value} FROM User")
+                sql.db.execute(f"SELECT name as {value} FROM User")
+                sql.User.query.filter_by(name=value).first()
+
+        @decorators.use_kwargs({"payload": fields.Str(required=True)}, location="query")
+        @decorators.endpoint(
+            path="/tests/vulnerabilities/<urlvalue>",
+            summary="Execute tests against known vulnerabilities",
+            description="Only enabled in testing mode",
+            responses={200: "Tests executed"},
+        )
+        def get(self, urlvalue: str, payload: str) -> Response:
+
+            self.do_queries(urlvalue)
+            self.payload(urlvalue)
+
+            return self.response(True)
+
+        @decorators.use_kwargs({"payload": fields.Str(required=True)})
+        @decorators.endpoint(
+            path="/tests/vulnerabilities/<urlvalue>",
+            summary="Execute tests against known vulnerabilities",
+            description="Only enabled in testing mode",
+            responses={200: "Tests executed"},
+        )
+        def post(self, urlvalue: str, payload: str) -> Response:
+
+            self.do_queries(urlvalue)
+            self.payload(urlvalue)
             return self.response(True)
--- tests/base/test_endpoints_vulnerabilities.py
+++ tests/base/test_endpoints_vulnerabilities.py
@@ -3,16 +3,26 @@ from restapi.tests import API_URI, FlaskClient
 
 def test_vulnerabilities(client: FlaskClient) -> None:
 
-    r = client.get(f"{API_URI}/tests/vulnerabilities/xx")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x'x")
-    r = client.get(f'{API_URI}/tests/vulnerabilities/x"x')
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x#x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x--x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x//x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x\\*x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x*x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x+x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x;x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x(x")
-    r = client.get(f"{API_URI}/tests/vulnerabilities/x)x")
-    assert r.status_code == 200
+    strings = (
+        "xx",
+        "x'x",
+        'x"x',
+        "x#x",
+        "x--x",
+        "x//x",
+        "x\\*x",
+        "x*x",
+        "x+x",
+        "x;x",
+        "x(x",
+        "x)x",
+    )
+
+    for s in strings:
+        r = client.get(
+            f"{API_URI}/tests/vulnerabilities/{s}", query_string={"payload": s}
+        )
+        assert r.status_code == 200
+
+        r = client.post(f"{API_URI}/tests/vulnerabilities/{s}", data={"payload": s})
+        assert r.status_code == 200
