5ef623b0491e834324d0969419c9c2a15a1c6db6
Mattia <m.dantonio81@gmail.com>
2021-05-31 22:08:42 +0200

    # Stub of vulnerabilities test

--- tests/base/test_endpoints_vulnerabilities.py
+++ tests/base/test_endpoints_vulnerabilities.py
@@ -3,16 +3,16 @@ from restapi.tests import API_URI, FlaskClient
 
 def test_vulnerabilities(client: FlaskClient) -> None:
 
-    r = client.post(f"{API_URI}/tests/vulnerabilities/xx")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x'x")
-    r = client.post(f'{API_URI}/tests/vulnerabilities/x"x')
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x#x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x--x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x//x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x\\*x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x*x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x+x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x;x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x(x")
-    r = client.post(f"{API_URI}/tests/vulnerabilities/x)x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/xx")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x'x")
+    r = client.get(f'{API_URI}/tests/vulnerabilities/x"x')
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x#x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x--x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x//x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x\\*x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x*x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x+x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x;x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x(x")
+    r = client.get(f"{API_URI}/tests/vulnerabilities/x)x")
     assert r.status_code == 200
