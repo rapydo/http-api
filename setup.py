8cbab26ad22165f18be562275f166bea80bbe0b8
Mattia <m.dantonio81@gmail.com>
2021-07-30 07:23:15 +0200

    Upgraded Faker from 8.9.0 to 8.10.3 (enabled type hints)

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
         "pytest-cov==2.12.1",
         "pytest-timeout==1.4.2",
         "schemathesis==3.9.2",
-        "Faker==8.9.0",
+        "Faker==8.10.3",
         "Telethon==1.22.0",
     ],
     classifiers=[
