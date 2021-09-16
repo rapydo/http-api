d2f8b99a3fa3c21682f23b78a7378c2fe5a79736
Mattia <m.dantonio81@gmail.com>
2021-09-16 07:43:40 +0200

    Bump Faker from 8.10.3 to 8.13.2

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
         "pytest-cov==2.12.1",
         "pytest-timeout==1.4.2",
         "schemathesis==3.10.0",
-        "Faker==8.10.3",
+        "Faker==8.13.2",
         "Telethon==1.22.0",
     ],
     classifiers=[
