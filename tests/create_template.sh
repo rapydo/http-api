85f797ce8b1d5dc4634d462a06cde70bf42fd8c3
Mattia <m.dantonio81@gmail.com>
2021-10-21 06:21:22 +0200

    # Fixed tests setup

--- tests/create_template.sh
+++ tests/create_template.sh
@@ -4,29 +4,31 @@ set -e
 TEMPLATE=$1
 AUTH=$2
 
+COMMON_OPTIONS=" --frontend no -e AUTH_LOGIN_BAN_TIME=10"
+
 if [[ "$TEMPLATE" == "postgres" ]]; then
-  rapydo create prj --auth postgres --frontend no -e AUTH_TOKEN_IP_GRACE_PERIOD=2
+  rapydo create prj --auth postgres ${COMMON_OPTIONS} -e AUTH_TOKEN_IP_GRACE_PERIOD=2
 elif [[ "$TEMPLATE" == "mysql" ]]; then
-  rapydo create prj --auth mysql --frontend no -e AUTH_TOKEN_IP_GRACE_PERIOD=2
+  rapydo create prj --auth mysql ${COMMON_OPTIONS} -e AUTH_TOKEN_IP_GRACE_PERIOD=2
 elif [[ "$TEMPLATE" == "neo4j" ]]; then
-  rapydo create prj --auth neo4j --frontend no -e AUTH_TOKEN_IP_GRACE_PERIOD=2
+  rapydo create prj --auth neo4j ${COMMON_OPTIONS} -e AUTH_TOKEN_IP_GRACE_PERIOD=2
 elif [[ "$TEMPLATE" == "mongo" ]]; then
-  rapydo create prj --auth mongo --frontend no -e AUTH_TOKEN_IP_GRACE_PERIOD=2
+  rapydo create prj --auth mongo ${COMMON_OPTIONS} -e AUTH_TOKEN_IP_GRACE_PERIOD=2
 elif [[ "$TEMPLATE" == "celery-rabbit-rabbit" ]]; then
-  rapydo create prj -s celery -s rabbit --auth ${AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit --auth ${AUTH} ${COMMON_OPTIONS};
 elif [[ "$TEMPLATE" == "celery-rabbit-redis" ]]; then
-  rapydo create prj -s celery -s rabbit -s redis --auth ${AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit -s redis --auth ${AUTH} ${COMMON_OPTIONS};
 elif [[ "$TEMPLATE" == "celery-rabbit-mongo" ]]; then
-  rapydo create prj -s celery -s rabbit -s mongo --auth ${AUTH} --frontend no;
+  rapydo create prj -s celery -s rabbit -s mongo --auth ${AUTH} ${COMMON_OPTIONS};
 elif [[ "$TEMPLATE" == "celery-redis-redis" ]]; then
-  rapydo create prj -s celery -s redis --auth ${AUTH} --frontend no;
+  rapydo create prj -s celery -s redis --auth ${AUTH} ${COMMON_OPTIONS};
 elif [[ "$TEMPLATE" == "celery-redis-mongo" ]]; then
-  rapydo create prj -s celery -s redis -s mongo --auth ${AUTH} --frontend no;
+  rapydo create prj -s celery -s redis -s mongo --auth ${AUTH} ${COMMON_OPTIONS};
 elif [[ "$TEMPLATE" == "noauth" ]]; then
-  rapydo create prj --auth no --frontend no
+  rapydo create prj --auth no ${COMMON_OPTIONS}
 
 elif [[ "$TEMPLATE" == "extra" ]]; then
-  rapydo create prjbase --auth ${AUTH} --frontend no -e PROXIED_CONNECTION=1
+  rapydo create prjbase --auth ${AUTH} ${COMMON_OPTIONS} -e PROXIED_CONNECTION=1
   # the --testing flag here (not included in others templates) will extend the
   # customizer class with additinal input/output fields to test these customizations
   # The flag is not included in all templates to be able to also tests the cases
@@ -36,7 +38,7 @@ elif [[ "$TEMPLATE" == "extra" ]]; then
                     --add-optionals \
                     --extend prjbase \
                     --auth ${AUTH} \
-                    --frontend no \
+                    ${COMMON_OPTIONS} \
                     -s ftp \
                     -s pushpin \
                     -s bot \
@@ -44,7 +46,6 @@ elif [[ "$TEMPLATE" == "extra" ]]; then
                     -e AUTH_MAX_PASSWORD_VALIDITY=60 \
                     -e AUTH_DISABLE_UNUSED_CREDENTIALS_AFTER=60 \
                     -e AUTH_MAX_LOGIN_ATTEMPTS=10 \
-                    -e AUTH_LOGIN_BAN_TIME=10 \
                     -e AUTH_SECOND_FACTOR_AUTHENTICATION=1 \
                     -e AUTH_TOKEN_IP_GRACE_PERIOD=2
 
