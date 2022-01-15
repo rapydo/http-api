3780fff80d67226351e22f670a9c3adf8d0af20e
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-01-15 17:53:39 +0100

    Disabled pushpin tests

--- tests/create_template.sh
+++ tests/create_template.sh
@@ -38,7 +38,7 @@ elif [[ "$TEMPLATE" == "extra" ]]; then
                     --auth ${AUTH} \
                     --frontend no \
                     -s ftp \
-                    -s pushpin \
+                    # -s pushpin \
                     -s bot \
                     -e AUTH_FORCE_FIRST_PASSWORD_CHANGE=1 \
                     -e AUTH_MAX_PASSWORD_VALIDITY=60 \
