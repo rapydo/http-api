84daa55821b6471ab966c53392c6600030598e15
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-11-13 09:05:41 +0100

    Typing fixes after the new no_implicit_optional=True default

--- restapi/connectors/celery/__init__.py
+++ restapi/connectors/celery/__init__.py
@@ -525,8 +525,8 @@ class CeleryExt(Connector):
         task: str,
         every: Union[str, int, timedelta],
         period: AllowedTimedeltaPeriods = "seconds",
-        args: List[Any] = None,
-        kwargs: Dict[str, Any] = None,
+        args: Optional[List[Any]] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
         if args is None:
             args = []
@@ -569,8 +569,8 @@ class CeleryExt(Connector):
         day_of_week: str = "*",
         day_of_month: str = "*",
         month_of_year: str = "*",
-        args: List[Any] = None,
-        kwargs: Dict[str, Any] = None,
+        args: Optional[List[Any]] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
 
         if args is None:
