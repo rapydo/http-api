1d63a7ba2d4803229838c3864bde60c3819895c9
Mattia D'Antonio <m.dantonio81@gmail.com>
2023-01-29 08:36:53 +0100

    Improve models definitions by using DeclarativeBase and mapped_column

--- restapi/connectors/sqlalchemy/models.py
+++ restapi/connectors/sqlalchemy/models.py
@@ -9,30 +9,33 @@ from sqlalchemy import (
     Table,
     Text,
 )
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import DeclarativeBase, mapped_column, relationship
 
-from restapi.connectors.sqlalchemy import db
+
+class Base(DeclarativeBase):
+    pass
+
 
 # Define multi-multi relation
 roles_users = Table(
     "roles_users",
-    db.metadata,
+    Base.metadata,
     Column("user_id", Integer, ForeignKey("user.id")),
     Column("role_id", Integer, ForeignKey("role.id")),
 )
 
 
 # Base type Model becomes "Any" due to an unfollowed import
-class Role(db):  # type: ignore
+class Role(Base):
     __tablename__ = "role"
 
     def __repr__(self) -> str:  # pragma: no cover
         name = self.__class__.__name__
         return f"{name}({self.id}, {self.name})"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), unique=True, nullable=False)
-    description = Column(String(255), nullable=False)
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(80), unique=True, nullable=False)
+    description = mapped_column(String(255), nullable=False)
 
     users = relationship(
         "User",
@@ -43,34 +46,34 @@ class Role(db):  # type: ignore
 
 
 # Base type Model becomes "Any" due to an unfollowed import
-class User(db):  # type: ignore
+class User(Base):  # type: ignore
     __tablename__ = "user"
 
     def __repr__(self) -> str:  # pragma: no cover
         name = self.__class__.__name__
         return f"{name}({self.id}, {self.email})"
 
-    id = Column(Integer, primary_key=True)
-    uuid = Column(String(36), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    name = Column(String(255, collation=None), nullable=False)
-    surname = Column(String(255, collation=None), nullable=False)
-    authmethod = Column(String(20), nullable=False)
-    password = Column(String(255), nullable=False)
-    mfa_hash = Column(String(255))
-    first_login = Column(DateTime(timezone=True))
-    last_login = Column(DateTime(timezone=True))
-    last_password_change = Column(DateTime(timezone=True))
-    is_active = Column(Boolean, default=True)
-    privacy_accepted = Column(Boolean, default=True)
-    expiration = Column(DateTime(timezone=True))
+    id = mapped_column(Integer, primary_key=True)
+    uuid = mapped_column(String(36), unique=True, nullable=False)
+    email = mapped_column(String(100), unique=True, nullable=False)
+    name = mapped_column(String(255, collation=None), nullable=False)
+    surname = mapped_column(String(255, collation=None), nullable=False)
+    authmethod = mapped_column(String(20), nullable=False)
+    password = mapped_column(String(255), nullable=False)
+    mfa_hash = mapped_column(String(255))
+    first_login = mapped_column(DateTime(timezone=True))
+    last_login = mapped_column(DateTime(timezone=True))
+    last_password_change = mapped_column(DateTime(timezone=True))
+    is_active = mapped_column(Boolean, default=True)
+    privacy_accepted = mapped_column(Boolean, default=True)
+    expiration = mapped_column(DateTime(timezone=True))
 
     roles = relationship(
         "Role",
         secondary=roles_users,
         back_populates="users",
     )  # type: ignore[var-annotated]
-    group_id = Column(Integer, ForeignKey("group.id"))
+    group_id = mapped_column(Integer, ForeignKey("group.id"))
     belongs_to = relationship(
         "Group",
         back_populates="members",
@@ -90,23 +93,23 @@ class User(db):  # type: ignore
 
 
 # Base type Model becomes "Any" due to an unfollowed import
-class Token(db):  # type: ignore
+class Token(Base):  # type: ignore
     __tablename__ = "token"
 
     def __repr__(self) -> str:  # pragma: no cover
         name = self.__class__.__name__
         return f"{name}({self.id})"
 
-    id = Column(Integer, primary_key=True)
-    jti = Column(String(36), unique=True, nullable=False)
-    token = Column(Text, nullable=False)
-    token_type = Column(String(1), nullable=False)
-    creation = Column(DateTime(timezone=True), nullable=False)
-    expiration = Column(DateTime(timezone=True))
-    last_access = Column(DateTime(timezone=True))
-    IP = Column(String(46))
-    location = Column(String(256))
-    user_id = Column(Integer, ForeignKey("user.id"))
+    id = mapped_column(Integer, primary_key=True)
+    jti = mapped_column(String(36), unique=True, nullable=False)
+    token = mapped_column(Text, nullable=False)
+    token_type = mapped_column(String(1), nullable=False)
+    creation = mapped_column(DateTime(timezone=True), nullable=False)
+    expiration = mapped_column(DateTime(timezone=True))
+    last_access = mapped_column(DateTime(timezone=True))
+    IP = mapped_column(String(46))
+    location = mapped_column(String(256))
+    user_id = mapped_column(Integer, ForeignKey("user.id"))
     emitted_for = relationship(
         "User",
         back_populates="tokens",
@@ -115,17 +118,17 @@ class Token(db):  # type: ignore
 
 
 # Base type Model becomes "Any" due to an unfollowed import
-class Group(db):  # type: ignore
+class Group(Base):  # type: ignore
     __tablename__ = "group"
 
     def __repr__(self) -> str:  # pragma: no cover
         name = self.__class__.__name__
         return f"{name}({self.id}, {self.shortname})"
 
-    id = Column(Integer, primary_key=True)
-    uuid = Column(String(36), unique=True, nullable=False)
-    shortname = Column(String(64), unique=True, nullable=False)
-    fullname = Column(String(256), nullable=False)
+    id = mapped_column(Integer, primary_key=True)
+    uuid = mapped_column(String(36), unique=True, nullable=False)
+    shortname = mapped_column(String(64), unique=True, nullable=False)
+    fullname = mapped_column(String(256), nullable=False)
 
     members = relationship(
         "User",
@@ -135,24 +138,24 @@ class Group(db):  # type: ignore
 
 
 # Base type Model becomes "Any" due to an unfollowed import
-class Login(db):  # type: ignore
+class Login(Base):  # type: ignore
     __tablename__ = "login"
 
     def __repr__(self) -> str:  # pragma: no cover
         name = self.__class__.__name__
         return f"{name}({self.id})"
 
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime(timezone=True), nullable=False)
+    id = mapped_column(Integer, primary_key=True)
+    date = mapped_column(DateTime(timezone=True), nullable=False)
     # same length of User.email
-    username = Column(String(100))
-    IP = Column(String(46))
-    location = Column(String(256))
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    username = mapped_column(String(100))
+    IP = mapped_column(String(46))
+    location = mapped_column(String(256))
+    user_id = mapped_column(Integer, ForeignKey("user.id"), nullable=True)
     user = relationship(
         "User",
         back_populates="logins",
         cascade_backrefs=False,
     )  # type: ignore[var-annotated]
-    failed = Column(Boolean, default=False)
-    flushed = Column(Boolean, default=False)
+    failed = mapped_column(Boolean, default=False)
+    flushed = mapped_column(Boolean, default=False)
